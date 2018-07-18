fcd1e7fbd93adf468cd362a21d8213626a1392ca
Matthew Stickle <mpstickle@ucdavis.edu>
2018-07-17 19:58:28 -0700

    Removed calculating the right and left sides and make it create the psth format for all neurons on the given file. Changed the parameters to take the parsed path, total bins, pre time, and post time

--- calculate_PSTH.m
+++ calculate_PSTH.m
@@ -1,7 +1,8 @@
-function [psth_path] = calculate_PSTH(parsed_path, edge)
-% Current default for bin and edge:
-% bin = 0.001;
-% edge = -0.2:bin:0.2;
+function [psth_path] = calculate_PSTH(parsed_path, total_bins, pre_time, post_time)
+% Current default values for testing:
+% total_bins = 400;
+% pre_time = 0.2;
+% post_time = 0.2;
     tic;
     parsed_mat_path = strcat(parsed_path, '/*.mat');
     parsed_files = dir([parsed_mat_path]);
@@ -15,34 +16,49 @@ function [psth_path] = calculate_PSTH(parsed_path, edge)
         file = strcat(parsed_path, '/');
         file = strcat(file, parsed_files(h).name);
         load(file);
-        % classifier window before time zero 
-        pretime=-.2;   %seconds
-
-        % classifier window after time zero 
-        posttime=.2;   %seconds
         
         %totalrelspikes is the (400 trials)x(Bins*Neurons) matrix which has each event trial for each
         %neuron with data put into 100 bins (-0.2 : 0.2) seconds.
         %Binned every 1 ms(see edge above)
 
         % Right PSTH
-        [right_rel_spikes_1]= Eventspiketimes(event1, right_spike_times, edge);
-        [right_rel_spikes_3]= Eventspiketimes(event3, right_spike_times, edge);
-        [right_rel_spikes_4]= Eventspiketimes(event4, right_spike_times, edge);
-        [right_rel_spikes_6]= Eventspiketimes(event6, right_spike_times, edge);
-        disp('Right PSTH Done');
+%         [right_rel_spikes_1]= Eventspiketimes(event1, right_spike_times, ...
+%             total_bins, pre_time, post_time);
+%         [right_rel_spikes_3]= Eventspiketimes(event3, right_spike_times, ...
+%             total_bins, pre_time, post_time);
+%         [right_rel_spikes_4]= Eventspiketimes(event4, right_spike_times, ...
+%             total_bins, pre_time, post_time);
+%         [right_rel_spikes_6]= Eventspiketimes(event6, right_spike_times, ...
+%             total_bins, pre_time, post_time);
+%         disp('Right PSTH Done');
+%         
+%         % Left PSTH
+%         [left_rel_spikes_1]= Eventspiketimes(event1, left_spike_times, ...
+%             total_bins, pre_time, post_time);
+%         [left_rel_spikes_3]= Eventspiketimes(event3, left_spike_times, ...
+%             total_bins, pre_time, post_time);
+%         [left_rel_spikes_4]= Eventspiketimes(event4, left_spike_times, ...
+%             total_bins, pre_time, post_time);
+%         [left_rel_spikes_6]= Eventspiketimes(event6, left_spike_times, ...
+%             total_bins, pre_time, post_time);
+%         disp('Left PSTH Done');
         
-        % Left PSTH
-        [left_rel_spikes_1]= Eventspiketimes(event1, left_spike_times, edge);
-        [left_rel_spikes_3]= Eventspiketimes(event3, left_spike_times, edge);
-        [left_rel_spikes_4]= Eventspiketimes(event4, left_spike_times, edge);
-        [left_rel_spikes_6]= Eventspiketimes(event6, left_spike_times, edge);
-        disp('Left PSTH Done');
+        % All PSTH
+        [all_rel_spikes_1]= Eventspiketimes(event1, all_spike_times, ...
+            total_bins, pre_time, post_time);
+        [all_rel_spikes_3]= Eventspiketimes(event3, all_spike_times, ...
+            total_bins, pre_time, post_time);
+        [all_rel_spikes_4]= Eventspiketimes(event4, all_spike_times, ...
+            total_bins, pre_time, post_time);
+        [all_rel_spikes_6]= Eventspiketimes(event6, all_spike_times, ...
+            total_bins, pre_time, post_time);
+        disp('All PSTH Done');
 
         %This next one is important-has spikes in bins
 
-        right_total_rel_spikes = [right_rel_spikes_1; right_rel_spikes_3; right_rel_spikes_4; right_rel_spikes_6];
-        left_total_rel_spikes = [left_rel_spikes_1; left_rel_spikes_3; left_rel_spikes_4; left_rel_spikes_6];        
+%         right_total_rel_spikes = [right_rel_spikes_1; right_rel_spikes_3; right_rel_spikes_4; right_rel_spikes_6];
+%         left_total_rel_spikes = [left_rel_spikes_1; left_rel_spikes_3; left_rel_spikes_4; left_rel_spikes_6];
+        all_total_rel_spikes = [all_rel_spikes_1; all_rel_spikes_3; all_rel_spikes_4; all_rel_spikes_6];
         
         %% Saving the file
         [pathstr,namestr,extstr] = fileparts(file);
@@ -50,7 +66,9 @@ function [psth_path] = calculate_PSTH(parsed_path, edge)
         filename = strcat(filename, '.mat');
         matfile = fullfile(psth_path, filename);
         
-        save(matfile, 'right_total_rel_spikes', 'left_total_rel_spikes');
+%         save(matfile, 'right_total_rel_spikes', 'left_total_rel_spikes', ...
+%             'total_left_neurons', 'total_right_neurons', 'all_total_rel_spikes');
+        save(matfile, 'all_total_rel_spikes', 'total_neurons');
     end
     toc;
 end
