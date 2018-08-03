9991afa8cec3e7f40b52a57e5a8cfea0dac1eeca
Matthew Stickle <mpstickle@ucdavis.edu>
2018-08-03 11:51:35 -0700

    Cleaned up the formation of the PSTH format. Restructured the final event_struct to be less cluttered and moved location of some loops to prevent repetitive calculations

--- calculate_PSTH.m
+++ calculate_PSTH.m
@@ -1,10 +1,11 @@
 % TODO change calculate_PSTH name to format_PSTH
 function [psth_path] = calculate_PSTH(parsed_path, animal_name, total_bins, bin_size, pre_time, post_time, ...
-                            wanted_neurons, wanted_events, trial_range)
+                            wanted_neurons, wanted_events, trial_range, total_trials)
     tic;
     % Grabs all .mat files in the parsed plx directory
     parsed_mat_path = strcat(parsed_path, '/*.mat');
     parsed_files = dir(parsed_mat_path);
+    wanted_events = sort(wanted_events);
     
     % Checks and creates a psth directory if it does not exists
     psth_path = strcat(parsed_path, '/psth');
@@ -25,6 +26,7 @@ function [psth_path] = calculate_PSTH(parsed_path, animal_name, total_bins, bin_
         event_strings{end+1} = ['event_', num2str(wanted_events(i))];
     end
     
+    
     for h = 1: length(parsed_files)
         failed_calculating = {};
         file = [parsed_path, '/', parsed_files(h).name];
@@ -33,45 +35,41 @@ function [psth_path] = calculate_PSTH(parsed_path, animal_name, total_bins, bin_
         current_day = seperated_file_name{4};
         fprintf('Calculating PSTH for %s on %s\n', animal_name, current_day);
         load(file);
-        event_struct = struct;
+
         try
-            for i = 1: length(wanted_events)
-                %% Slices out the desired neurons from all_spike_times and puts them into
-                %% the neuron matrix
-                neurons = [];
-                if isempty(wanted_neurons)
-                    neurons = all_spike_times;
-                else
-                    for neuron = length(wanted_neurons)
-                        neurons = [neurons; all_spike_times(wanted_neurons(neuron), :)];
-                    end
+            event_struct = struct;
+            %% Slices out the desired neurons from all_spike_times and puts them into
+            %% the neuron matrix
+            neurons = [];
+            if isempty(wanted_neurons)
+                neurons = neuron_map(:,2);
+            else
+                for neuron = length(wanted_neurons)
+                    neurons = [neurons; neuron_map(wanted_neurons(neuron), 2)];
                 end
-                %% Slices out the desired trials from the events matrix (Inclusive range)
-                events = events(trial_range(1):trial_range(2), :);
-                %% Selects the desired events from the events matrix and puts them into an event_struct
-                event_struct.(event_strings{i}) = events(find(events == wanted_events(i)), 2);
-                event_struct.('total_count') = tabulate(events(:,1));
-                %% Creates the psth format and adds them to the event_struct
-                event_struct.([event_strings{i}, '_rel_spikes']) = ...
-                    event_spike_times(event_struct.(event_strings{i}), ...
-                    neurons, total_bins, bin_size, pre_time, post_time);
-                event_struct.([event_strings{i}, '_raster']) = ...
-                    sum(event_struct.([event_strings{i}, '_rel_spikes']), 1);
             end
+            % Truncates events to desired trial range from total_trials * total_events
+            events = events(trial_range(1):trial_range(2), :);
             
-            %% Concates all relative events together into a combined matrix of all events
-            % Total relative spikes is the (# trials)x(bins*neurons) matrix
-            % which has each event trial for each neuron with data put in the #
-            % of total bins defined by the window given by the pre and post
-            % times and stepped by the bin size
-            struct_names = fieldnames(event_struct);
-            combined_rel_event_spikes = [];
-            for i = 1: length(struct_names)
-                if contains(struct_names{i}, '_rel_spikes')
-                    event_struct.combined_rel_event_spikes = ...
-                        [combined_rel_event_spikes; getfield(event_struct, struct_names{i})];                            
-                end
+            event_struct.all_events = {};
+            for i = 1: length(wanted_events)
+                %% Slices out the desired trials from the events matrix (Inclusive range)
+                event_struct.all_events = [event_struct.all_events; event_strings{i}, {events(events == wanted_events(i), 2)}];
             end
+
+            event_struct.relative_response = event_spike_times(neurons, event_struct.all_events(:,2), ...
+                total_trials, total_bins, bin_size, pre_time, post_time);
+            % TODO verify PSTH numbers against plx software
+            event_struct.event_count = tabulate(event_struct.all_events(:,1));
+            events_array = event_struct.all_events(:,2);
+            event_count = 0;
+            for event = 1: length(events_array)
+                event_struct.([event_strings{event}, '_raster']) = ...
+                sum(event_struct.relative_response((event_count + 1):1:(event_count + length(events_array{i})),:),1);
+                % Updates event_count to scale sum properly for next row
+                event_count = event_count + length(events_array{i});
+            end
+
             fprintf('Finished PSTH for %s\n', current_day);
             %% Saving the file
             filename = ['PSTH.format.', file_name, '.mat'];
