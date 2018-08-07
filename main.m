73dd72940917d7f79f648f8dff6162d401543b10
Matthew Stickle <mpstickle@ucdavis.edu>
2018-08-06 18:26:44 -0700

    Added wanted_neurons to crude_bootstrapping call

--- main.m
+++ main.m
@@ -20,7 +20,7 @@ function [] = main()
     % Default is set to single neuron
     unit_classification = true;
     % controls how many bootstrap iterations are done. Default is 1 (equivalent to single classification)
-    boot_iterations = 50;
+    boot_iterations = 5;
 
     % Get the directory with all animals and their respective .plx files
     original_path = uigetdir(pwd);
@@ -35,23 +35,23 @@ function [] = main()
                 continue;
             else
                 %% Run if you want to parse .plx or comment out to skip
-                try
-                    parsed_path = parser(animal_path, animal_name, total_trials, total_events);
-                catch
-                    failed{end+1} = animal_list(animal).name;
-                end
+                % try
+                %     parsed_path = parser(animal_path, animal_name, total_trials, total_events);
+                % catch
+                %     failed{end+1} = animal_list(animal).name;
+                % end
                 %% Use the code commented out below to skip parsing
-                % parsed_path = [animal_path, '/parsed_plx'];
+                parsed_path = [animal_path, '/parsed_plx'];
 
                 %% Run if you want to calculate the PSTH or comment it out to skip
-                try
-                    psth_path = calculate_PSTH(parsed_path, animal_name, total_bins, bin_size, pre_time, post_time, ...
-                        wanted_neurons, wanted_events, trial_range, total_trials);
-                catch
-                    failed{end+1} = animal_list(animal).name;
-                end
+                % try
+                %     psth_path = calculate_PSTH(parsed_path, animal_name, total_bins, bin_size, pre_time, post_time, ...
+                %         wanted_neurons, wanted_events, trial_range, total_trials);
+                % catch
+                %     failed{end+1} = animal_list(animal).name;
+                % end
                 %% Use code commeneted out below to skip PSTH calculations
-                % psth_path = [parsed_path, '/psth'];
+                psth_path = [parsed_path, '/psth'];
 
                 %% Run if you want to graph all of the PSTHs or comment it out to skip
                 % try
@@ -62,7 +62,7 @@ function [] = main()
 
                 %% Run for bootstrapping
                 classified_path = crude_bootstrapper(psth_path, animal_name, boot_iterations, bin_size, pre_time, ...
-                    post_time, wanted_events, unit_classification);
+                    post_time, wanted_events, wanted_neurons, unit_classification);
             end
         end
     end
