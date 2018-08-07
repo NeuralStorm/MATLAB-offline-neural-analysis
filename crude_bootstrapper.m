f55240718775b18b239c26d1d2e84e9e1942508a
Matthew Stickle <mpstickle@ucdavis.edu>
2018-08-06 17:23:06 -0700

    Crude_bootstrapping handles bootstrapping for a specified amount of iterations and outputs the given info in a classified struct. crude_bbootstrapping makes use of calling crude_classifier to do the classification portion.

---
+++ crude_bootstrapper.m
@@ -0,0 +1,79 @@
+function [classify_path] = crude_bootstrapper(psth_path, animal_name, boot_iterations, bin_size, pre_time, post_time, wanted_events, unit_classification)
+    % Grabs all the psth formatted files
+    psth_mat_path = strcat(psth_path, '/*.mat');
+    psth_files = dir(psth_mat_path);
+
+    % Checks if a classify graph directory exists and if not it creates it
+    classify_path = strcat(psth_path, '/classifier');
+    if ~exist(classify_path, 'dir')
+        mkdir(psth_path, 'classifier');
+    end
+
+     % Creates a directory to store the failed files
+     failed_path = [classify_path, '/failed'];
+     if ~exist(failed_path, 'dir')
+         mkdir(classify_path, 'failed');
+     else
+         delete([failed_path, '/*']);
+     end
+
+    %% Iterates through all the psth formated files to for classifiers
+    for h = 1: length(psth_files)
+        failed_classifying = {};
+        file = [psth_path, '/', psth_files(h).name];
+        [file_path, file_name, file_extension] = fileparts(file);
+        split_name = strsplit(file_name, '.');
+        current_day = split_name{6};
+        fprintf('Bootstrapping PSTH for %s on %s\n', animal_name, current_day);
+        try
+            load(file);
+            for i = 1: boot_iterations
+                disp(i);
+                if i == 1
+                    classified_struct = struct;
+                    % Initialize dynamic struct fields
+                    classified_struct.deciscion = {};
+                    classified_struct.true_event = {};
+                    classified_struct.correct_trials = [];
+
+                    % Preforms standard classification
+                    classified_struct = crude_classifier(failed_path, file_name, event_struct.all_events, neuron_map, bin_size, pre_time, post_time, unit_classification, i, classified_struct);
+                else
+                    % Shuffle event labels from the events matrix
+                    shuffled_event_labels = events(:,1);
+                    shuffled_event_labels = shuffled_event_labels(randperm(length(shuffled_event_labels)));
+                    shuffled_events = [shuffled_event_labels, events(:,2)];
+                    % Recreate the event cell array for PSTH object
+                    all_events = {};
+                    for event = 1: length(wanted_events)
+                        %% Slices out the desired trials from the events matrix (Inclusive range)
+                        all_events = [all_events; event_strings{event}, {shuffled_events(shuffled_events == wanted_events(event), 2)}];
+                    end
+                    classified_struct = crude_classifier(failed_path, file_name, all_events, neuron_map, bin_size, pre_time, post_time, unit_classification, i, classified_struct);
+                end
+            end
+
+            struct_names = fieldnames(classified_struct);
+            for i = 1: length(struct_names)
+                if contains(struct_names{i}, '_bootstrapped_info')
+                    classified_struct.(struct_names{i}) = mean(classified_struct.(struct_names{i}));
+                end
+            end
+
+            %% Saving classifier info
+            % fprintf('Finished classifying for %s\n', current_day);
+            all_events = event_struct.all_events;
+            filename = ['CLASSIFIED.', file_name, '.mat'];
+            matfile = fullfile(classify_path, filename);
+            save(matfile, 'classified_struct', 'neuron_map', 'all_events');
+        catch ME
+            failed_classifying{end + 1} = file_name;
+            failed_classifying{end, 2} = ME;
+            filename = ['FAILED.', file_name, '.mat'];
+            warning('%s failed to bootstrap\n', file_name);
+            warning('Error: %s\n', ME.message);
+            matfile = fullfile(failed_path, filename);
+            save(matfile, 'failed_bootstrapping');
+        end
+    end
+end
