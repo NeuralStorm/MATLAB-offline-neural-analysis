1741f7db1202f08d0480e725e8b31301a7862aeb
Matthew Stickle <mpstickle@ucdavis.edu>
2018-08-14 14:56:25 -0700

    Goes through the psth files produced by calculate_PSTH.m and adds a direct and indirect neuron map

---
+++ label_neurons.m
@@ -0,0 +1,27 @@
+function [unit_index] = label_neurons(psth_path, neuron_labels, unit_index)
+    % Grabs all the psth formatted files
+    psth_mat_path = strcat(psth_path, '/*.mat');
+    psth_files = dir(psth_mat_path);
+
+    for h = 1: length(psth_files)
+        file = [psth_path, '/', psth_files(h).name];
+        [~, name_str, ~] = fileparts(file);
+        split_name = strsplit(name_str, '.');
+        current_day = split_name{6};
+        day_path = [graph_path, '/', current_day];
+        load(file);
+        direct_neurons = {};
+        indirect_neurons = {};
+        for neuron = 1: total_neurons
+            if neuron_labels(neuron) == 'Direct'
+                direct_neurons{end + 1} = neuron_map(neuron, :);
+            else
+                indirect_neurons{end + 1} = neuron_map(neuron, :);
+            end
+            unit_index = unit_index + 1;
+        end
+        matfile = fullfile(psth_path, name_str);
+        save(matfile, 'event_struct', 'total_neurons', 'neuron_map', 'events', 'event_strings', 'direct_neurons', 'indirect_neurons');
+        disp(unit_index);
+    end
+end
