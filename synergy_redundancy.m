a733099bf9ab5db57132b3381d3764b916bd9269
Matthew Stickle <mpstickle@ucdavis.edu>
2018-08-15 14:20:26 -0700

    Fixed the synergy redundancy calculation to handle the right and left neuron groups when calculating synergy redundancy

--- synergy_redundancy.m
+++ synergy_redundancy.m
@@ -19,25 +19,33 @@ function [] = synergy_redundancy(classified_path, animal_name)
         %% Grab the sum of the corrected unit info
         load(unit_file);
         struct_names = fieldnames(classified_struct);
-        unit_corrected_sum = 0;
+        left_corrected_sum = 0;
+        right_corrected_sum = 0;
         for i = 1: length(struct_names)
             if contains(struct_names{i}, '_information')
+                unit = strsplit(struct_names{i}, '_');
+                unit_name = unit{1};
+                unit_number = str2num(unit_name(4:6));
                 unit_info = getfield(classified_struct, struct_names{i});
-                if unit_info > 0
-                    unit_corrected_sum = unit_corrected_sum + unit_info;
-                else
-                    continue;
+                if (unit_info > 0) && (unit_number > 16)
+                    left_corrected_sum = left_corrected_sum + unit_info;
+                elseif (unit_info > 0) && (unit_number <= 16)
+                    right_corrected_sum = right_corrected_sum + unit_info;
                 end
             end
         end
 
         %% Grab the population information
         load(pop_file);
-        corrected_pop_info = classified_struct.population_corrected_info;
-        syn_red = corrected_pop_info - unit_corrected_sum;
-        classified_struct.syn_red_value = syn_red;
+        corrected_right_info = classified_struct.right_corrected_info;
+        corrected_left_info = classified_struct.left_corrected_info;
+        right_syn_red = corrected_right_info - right_corrected_sum;
+        left_syn_red = corrected_left_info - left_corrected_sum;
+        classified_struct.right_syn_red = right_syn_red;
+        classified_struct.left_syn_red = left_syn_red;
         % TODO what happens when syn red is 0?
-        classified_struct.syn_red_bool = syn_red > 0;
+        classified_struct.right_syn_red_bool = right_syn_red > 0;
+        classified_struct.left_syn_red_bool = left_syn_red > 0;
         save(pop_file, 'classified_struct', 'neuron_map', 'all_events', 'total_neurons');
     end
 end
