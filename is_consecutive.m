10e6b9469b2c81667220720e12a9ca2c543f8dd8
Matthew Stickle <mpstickle@ucdavis.edu>
2018-08-30 13:42:44 -0700

    Changed the way of finding consecutive bins by checking the difference vector. Still returns a boolean if enough consecutive bins are found above threshold as well as return the index of the first set of consecutive bins

--- is_consecutive.m
+++ is_consecutive.m
@@ -1,16 +1,21 @@
-function [consecutive] = is_consecutive(above_threshold_indeces, sig_bins)
+function [consecutive, bin_indeces] = is_consecutive(above_threshold_indeces, sig_bins)
     consecutive_bins = 0;
     consecutive = false;
+    bin_indeces = [];
     if length(above_threshold_indeces) >= sig_bins
-        for bin = 1:(length(above_threshold_indeces) - 1)
-            if above_threshold_indeces(bin + 1) - above_threshold_indeces(bin) == 1
+        difference = diff(above_threshold_indeces);
+        for i = 1:length(difference)
+            if difference(i) == 1
                 consecutive_bins = consecutive_bins + 1;
+                bin_indeces = [bin_indeces; above_threshold_indeces(i)];
                 if consecutive_bins >= sig_bins
                     consecutive = true;
-                    break;
                 end
+            elseif consecutive
+                break;
             else
                 consecutive_bins = 0;
+                bin_indeces = [];
             end
         end
     end
