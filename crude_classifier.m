6ace524c899af16f1c007e15aaaec1a1f61bb2c7
Matthew Stickle <mpstickle@ucdavis.edu>
2018-09-04 14:44:10 -0700

    Removed hard coding of left and right neurons. The labels for the neurons are determined by a .csv that is applied to the channels in label_neurons.m. The code now iterates through all the unique regions and does classification for each unique region.

--- crude_classifier.m
+++ crude_classifier.m
@@ -1,4 +1,4 @@
-function [classified_struct] = crude_classifer(failed_path, file_name, all_events, neuron_map, right_neurons, left_neurons, bin_size, pre_time, post_time, unit_classification, iteration, classified_struct)
+function [classified_struct] = crude_classifer(failed_path, file_name, all_events, neuron_map, labeled_neurons, bin_size, pre_time, post_time, unit_classification, iteration, classified_struct)
     %% Crude classifier
     if pre_time ~= 0
         warning('The classifier will try and classify on the time period before the event took place. Consider changing pre time to 0');
@@ -39,63 +39,34 @@ function [classified_struct] = crude_classifer(failed_path, file_name, all_event
                 end
             end
         else
-            %% Right neurons
-            % Creates the PSTH object using dark, unknown magic from mythical toolbox
-            right_psth = NeuroToolbox.PSTHToolbox.PSTH(right_neurons, all_events, 'bin_size', ... 
-                bin_size, 'PSTH_window', [-abs(pre_time), post_time]);
-            % create template from PSTH object using more dark magic
-            right_template = NeuroToolbox.PSTHToolbox.SU_Classifier(right_psth);
-            % peform classification using template with more excitement and fun...
-            right_decoder_output = right_template.classify(right_neurons, all_events, 'SameDataSet', true);
-            %% Left neurons
-            % Creates the PSTH object using dark, unknown magic from mythical toolbox
-            left_psth = NeuroToolbox.PSTHToolbox.PSTH(left_neurons, all_events, 'bin_size', ... 
-                bin_size, 'PSTH_window', [-abs(pre_time), post_time]);
-            % create template from PSTH object using more dark magic
-            left_template = NeuroToolbox.PSTHToolbox.SU_Classifier(left_psth);
-            % peform classification using template with more excitement and fun...
-            left_decoder_output = left_template.classify(left_neurons, all_events, 'SameDataSet', true);
-            if iteration == 1
-                % Initialize dynamic struct fields
-                classified_struct.right_deciscion = {};
-                classified_struct.right_true_event = {};
-                classified_struct.right_correct_trials = [];
-                classified_struct.left_deciscion = {};
-                classified_struct.left_true_event = {};
-                classified_struct.left_correct_trials = [];
-                %% Right neurons
-                classified_struct.right_decorder = right_decoder_output;
-                classified_struct.right_deciscion = [classified_struct.right_deciscion, right_decoder_output.Decision];
-                classified_struct.right_true_event = [classified_struct.right_true_event, right_decoder_output.Event];
-                right_correct_trials = cellfun(@strcmp, right_decoder_output.Decision, right_decoder_output.Event);
-                classified_struct.right_correct_trials = [classified_struct.right_correct_trials, right_correct_trials];
-                classified_struct.right_confusion = confusionmat(right_decoder_output.Event, right_decoder_output.Decision);
-                classified_struct.right_accuracy = mean(right_correct_trials);
-                classified_struct.right_psth_object = right_psth;
-                classified_struct.right_overall_classification = [classified_struct.right_true_event, classified_struct.right_deciscion, num2cell(classified_struct.right_correct_trials)];
-                classified_struct.right_information = I_confmatr(classified_struct.right_confusion);
-                classified_struct.right_bootstrapped_info = [];
-                %% Left Neurons
-                classified_struct.left_decorder = left_decoder_output;
-                classified_struct.left_deciscion = [classified_struct.left_deciscion, left_decoder_output.Decision];
-                classified_struct.left_true_event = [classified_struct.left_true_event, left_decoder_output.Event];
-                left_correct_trials = cellfun(@strcmp, left_decoder_output.Decision, left_decoder_output.Event);
-                classified_struct.left_correct_trials = [classified_struct.left_correct_trials, left_correct_trials];
-                classified_struct.left_confusion = confusionmat(left_decoder_output.Event, left_decoder_output.Decision);
-                classified_struct.left_accuracy = mean(left_correct_trials);
-                classified_struct.left_psth_object = left_psth;
-                classified_struct.left_overall_classification = [classified_struct.left_true_event, classified_struct.left_deciscion, num2cell(classified_struct.left_correct_trials)];
-                classified_struct.left_information = I_confmatr(classified_struct.left_confusion);
-                classified_struct.left_bootstrapped_info = [];
-            else
-                %% Right Neurons
-                right_bootstrapped_confusion = confusionmat(right_decoder_output.Event, right_decoder_output.Decision);
-                right_bootstrapped_info = I_confmatr(right_bootstrapped_confusion);
-                classified_struct.right_bootstrapped_info = [classified_struct.right_bootstrapped_info; right_bootstrapped_info];
-                %% Left Neurons
-                left_bootstrapped_confusion = confusionmat(left_decoder_output.Event, left_decoder_output.Decision);
-                left_bootstrapped_info = I_confmatr(left_bootstrapped_confusion);
-                classified_struct.left_bootstrapped_info = [classified_struct.left_bootstrapped_info; left_bootstrapped_info];
+            region_names = fieldnames(labeled_neurons);
+            for region = 1:length(region_names)
+                region_neurons = [labeled_neurons.(region_names{region})(:,1), labeled_neurons.(region_names{region})(:,end)];
+                region_psth = NeuroToolbox.PSTHToolbox.PSTH(region_neurons, all_events, 'bin_size', ... 
+                    bin_size, 'PSTH_window', [-abs(pre_time), post_time]);
+                region_template = NeuroToolbox.PSTHToolbox.SU_Classifier(region_psth);
+                region_decoder_output = region_template.classify(region_neurons, all_events, 'SameDataSet', true);
+                if iteration == 1
+                    % Initialize dynamic struct fields
+                    classified_struct.([region_names{region}, '_deciscion']) = {};
+                    classified_struct.([region_names{region}, '_true_event']) = {};
+                    classified_struct.([region_names{region}, '_correct_trials']) = [];
+                    classified_struct.([region_names{region}, '_decorder']) = region_decoder_output;
+                    classified_struct.([region_names{region}, '_deciscion']) = [classified_struct.([region_names{region}, '_deciscion']), region_decoder_output.Decision];
+                    classified_struct.([region_names{region}, '_true_event']) = [classified_struct.([region_names{region}, '_true_event']), region_decoder_output.Event];
+                    correct_trials = cellfun(@strcmp, region_decoder_output.Decision, region_decoder_output.Event);
+                    classified_struct.([region_names{region}, '_correct_trials']) = [classified_struct.([region_names{region}, '_correct_trials']), correct_trials];
+                    classified_struct.([region_names{region}, '_confusion']) = confusionmat(region_decoder_output.Event, region_decoder_output.Decision);
+                    classified_struct.([region_names{region}, '_accuracy']) = mean(correct_trials);
+                    classified_struct.([region_names{region}, '_psth_object']) = region_psth;
+                    classified_struct.([region_names{region}, '_overall_classification']) = [classified_struct.([region_names{region}, '_true_event']), classified_struct.([region_names{region}, '_deciscion']), num2cell(classified_struct.([region_names{region}, '_correct_trials']))];
+                    classified_struct.([region_names{region}, '_information']) = I_confmatr(classified_struct.([region_names{region}, '_confusion']));
+                    classified_struct.([region_names{region}, '_bootstrapped_info']) = [];
+                else
+                    region_bootstrapped_confusion = confusionmat(region_decoder_output.Event, region_decoder_output.Decision);
+                    region_bootstrapped_info = I_confmatr(region_bootstrapped_confusion);
+                    classified_struct.([region_names{region}, '_bootstrapped_info']) = [classified_struct.([region_names{region}, '_bootstrapped_info']); region_bootstrapped_info];
+                end
             end
         end
     catch ME
