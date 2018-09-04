4ec063b0fe33d791b0f3b5fcad52a00738c4657d
Matthew Stickle <mpstickle@ucdavis.edu>
2018-09-04 14:46:07 -0700

    Removed hard coding of synergy redundancy and changed it to do a synergy redundancy calculation per region defined in a .csv that is read in and applied to the channels in label_neurons.m

--- synergy_redundancy.m
+++ synergy_redundancy.m
@@ -16,33 +16,30 @@ function [] = synergy_redundancy(classified_path, animal_name)
 
         %% Grab the sum of the corrected unit info
         load(unit_file);
-        struct_names = fieldnames(classified_struct);
-        left_corrected_sum = 0;
-        right_corrected_sum = 0;
-        for i = 1: length(struct_names)
-            if contains(struct_names{i}, '_information')
-                unit = strsplit(struct_names{i}, '_');
-                unit_name = unit{1};
-                unit_number = str2num(unit_name(4:6));
-                unit_info = getfield(classified_struct, struct_names{i});
-                if (unit_info > 0) && (unit_number > 16)
-                    left_corrected_sum = left_corrected_sum + unit_info;
-                elseif (unit_info > 0) && (unit_number <= 16)
-                    right_corrected_sum = right_corrected_sum + unit_info;
+
+        classified_struct_names = fieldnames(classified_struct);
+        region_names = fieldnames(labeled_neurons);
+        %% Iterates through all the regions
+        for region = 1:length(region_names)
+            region_struct.([region_names{region}, '_sum']) = 0;
+            %% Iterates through all the channels within a given region
+            for neuron = 1:length(labeled_neurons.(region_names{region}))
+                unit_info = getfield(classified_struct, [labeled_neurons.(region_names{region}){neuron}, '_information']);
+                % Adds up the total unit information when info is above 0
+                if unit_info > 0
+                    region_struct.([region_names{region}, '_sum']) = region_struct.([region_names{region}, '_sum']) + unit_info;
                 end
             end
         end
 
-        %% Grab the population information
+        %% Grab population information
         load(pop_file);
-        corrected_right_info = classified_struct.right_corrected_info;
-        corrected_left_info = classified_struct.left_corrected_info;
-        right_syn_red = corrected_right_info - right_corrected_sum;
-        left_syn_red = corrected_left_info - left_corrected_sum;
-        classified_struct.right_syn_red = right_syn_red;
-        classified_struct.left_syn_red = left_syn_red;
-        classified_struct.right_syn_red_bool = right_syn_red > 0;
-        classified_struct.left_syn_red_bool = left_syn_red > 0;
-        save(pop_file, 'classified_struct', 'neuron_map', 'all_events', 'total_neurons');
+        for region = 1:length(region_names)
+            corrected_region_info = classified_struct.([region_names{region}, '_corrected_info']);
+            region_syn_red = corrected_region_info - region_struct.([region_names{region}, '_sum']);
+            classified_struct.([region_names{region}, '_syn_red']) = region_syn_red;
+            classified_struct.([region_names{region}, '_syn_red_bool']) = region_syn_red > 0;
+        end
+        save(pop_file, 'classified_struct', 'neuron_map', 'all_events', 'total_neurons', 'labeled_neurons');
     end
 end
