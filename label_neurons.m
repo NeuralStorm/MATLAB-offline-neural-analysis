7cc65299f4358d35046ad0e2967dd2ee5797c170
Matthew Stickle <mpstickle@ucdavis.edu>
2018-09-04 14:42:09 -0700

    label_neurons.m now reads in a .csv and applies the label keys along with the label values to a neuron map instead of hard coding in different regions.

--- label_neurons.m
+++ label_neurons.m
@@ -1,10 +1,20 @@
-function [] = label_neurons(psth_path)
-    % Grabs all the psth formatted files
+function [] = label_neurons(animal_path, psth_path)
+    %% Grabs all the psth formatted files
     psth_mat_path = strcat(psth_path, '/*.mat');
     psth_files = dir(psth_mat_path);
+    %% Grabs label file
+    animal_csv_path = [animal_path, '/*.csv'];
+    csv_files = dir(animal_csv_path);
+    for csv = 1:length(csv_files)
+        csv_file = fullfile(animal_path, csv_files(csv).name);
+        if contains(csv_files(csv).name, 'labels.csv')
+            labels = readtable(csv_file);
+            unique_regions = unique(labels.(2));
+        end
+    end
 
     for h = 1: length(psth_files)
-        % PSTH.format.TNC.25.ClosedLoop.Day26.111416
+        %% Load psth formatted data
         file = [psth_path, '/', psth_files(h).name];
         [~, name_str, ~] = fileparts(file);
         split_name = strsplit(name_str, '.');
@@ -12,21 +22,18 @@ function [] = label_neurons(psth_path)
         current_animal_num = split_name{4};
         current_day = split_name{6};
         load(file);
-        right_neurons = {};
-        left_neurons = {};
-        for neuron = 1: total_neurons
-            channel_name = neuron_map{neuron, 1};
-            channel_num = str2num(channel_name(4:6));
-            if channel_num > 16
-                % Assigns to left neurons
-                left_neurons(end + 1, 1) = neuron_map(neuron, 1);
-                left_neurons(end, 2) = neuron_map(neuron, 2);
-            else
-                % Assigns to right neurons
-                right_neurons(end + 1, 1) = neuron_map(neuron, 1);
-                right_neurons(end, 2) = neuron_map(neuron, 2);
-            end
+        %% Creates the label struct for each file
+        for region = 1:length(unique_regions)
+            % Seperate out specific region index in table. IE: only indeces for left neurons in the .csv
+            region_name_indeces = strcmpi(labels.(2), unique_regions{region});
+            channels = labels.(1)(region_name_indeces);
+            % Find the channels in the neuron_map that actually have data
+            [shared_channels, map_indeces, channels_indeces] = intersect(neuron_map(:,1), channels);
+            regions = labels.(2)(channels_indeces);
+            region_values = num2cell(labels.(3)(channels_indeces));
+            labeled_neurons.(unique_regions{region}) = horzcat(shared_channels, regions, region_values, neuron_map(map_indeces,2));
         end
-        save(file, 'event_struct', 'total_neurons', 'neuron_map', 'events', 'event_strings', 'right_neurons', 'left_neurons');
+
+        save(file, 'event_struct', 'total_neurons', 'neuron_map', 'events', 'event_strings', 'labeled_neurons');
     end
 end
