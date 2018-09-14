611dac01db40edf400ad539838a8600e13ade044
Matthew Stickle <mpstickle@ucdavis.edu>
2018-09-14 10:38:48 -0700

    Added event strings to the saved matfile with all the results from the receptive field analysis.

--- receptive_field_analysis.m
+++ receptive_field_analysis.m
@@ -179,6 +179,6 @@ function [rf_path] = receptive_field_analysis(psth_path, animal_name, pre_time, 
         rf_filename = strrep(filename, 'PSTH', 'REC');
         rf_filename = strrep(rf_filename, 'format', 'FIELD');
         matfile = fullfile(rf_path, [rf_filename, '.mat']);
-        save(matfile, 'receptive_analysis', 'labeled_neurons', 'normalized_variance');
+        save(matfile, 'receptive_analysis', 'labeled_neurons', 'normalized_variance', 'event_strings');
     end
 end
