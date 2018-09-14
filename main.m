a0369ea78e2b672646266f510b875f35c0a2604a
Matthew Stickle <mpstickle@ucdavis.edu>
2018-09-14 10:38:02 -0700

    Added normalized variance analysis on a per animal basis and updated a lot of the comments and general formatting of main. More improvements to come. Current parameters are set to do the receptive field and normalized variance analysis.

--- main.m
+++ main.m
@@ -1,7 +1,11 @@
 function [] = main()
     start_time = tic;
     %% Initialize global variables
-    bin_size = 0.002; total_trials = 100; total_events = 4; pre_time = 0; post_time = 0.2;
+    bin_size = 0.002;
+    total_trials = 100;
+    total_events = 4;
+    pre_time = 0.2;
+    post_time = 0.2;
     total_bins = (length([-abs(pre_time):bin_size:abs(post_time)]) - 1);
     % Requires for all events to be in array. IF empty it will skip all events
     wanted_events = [1, 3, 4, 6];
@@ -10,20 +14,18 @@ function [] = main()
     % Inclusive Range
     trial_range = [1, 300];
     % Give exact directory name of the animals you want skipped
-    ignored_animals = ['PRAC03', 'TNC01', 'TNC14', 'TNC16', 'TNC25'];
+    ignored_animals = ['LC02', 'TNC01', 'TNC14', 'TNC16', 'TNC25'];
     % Boolean to control classification for population or single neurons
     % Default is set to single neuron
-    unit_classification = false;
+    unit_classification = true;
     % controls how many bootstrap iterations are done. Default is 1 (equivalent to single classification)
-    boot_iterations = 5;
+    boot_iterations = 1;
     spreadsheet_name = 'unit_20ms_spreadsheet.csv';
     append_spreadsheet = false;
 
     %% Receptive Field Analysis
-    rf_analysis = false;
-    % Sets the coefficents used for smoothing the PSTHs for each neuron
-    %TODO determine to use filter or smooth function
-    %% smooth parameters
+    rf_analysis = true;
+    % Span is the number of bins, centered on the current bin the moving average filter will be applied to
     span = 3;
     % threshold_scale determines how the threshold is scaled
     % avg background activity + threshold_scale * standard deviation(background activity)
@@ -33,7 +35,8 @@ function [] = main()
     sig_check = 1;
     % sig_bins determines how many consecutive bins are needed for significant response
     sig_bins = 5;
-    % Sets the filter window to desired length
+    % List of where all the nv analysis result files are stored for population analysis at the end
+    nv_list = [];
 
     
     % Get the directory with all animals and their respective .plx files
@@ -58,24 +61,27 @@ function [] = main()
                 parsed_path = [animal_path, '/parsed_plx'];
 
                 %% Run if you want to calculate the PSTH or comment it out to skip
-                % try
+                try
                     psth_path = format_PSTH(parsed_path, animal_name, total_bins, bin_size, pre_time, post_time, ...
                         wanted_neurons, wanted_events, trial_range, total_trials);
-                    label_neurons(animal_path, psth_path);
-                % end
+                    unique_regions = label_neurons(animal_path, psth_path);
+                end
                 %% Use code commeneted out below to skip PSTH calculations
                 psth_path = [parsed_path, '/psth'];
 
                 %% Use to run receptive field analysis
                 if rf_analysis
-                    % try
+                    try
                         rf_path = receptive_field_analysis(psth_path, animal_name, pre_time, post_time, bin_size, total_bins, ...
                             threshold_scale, sig_check, sig_bins, span, wanted_events);
-                    % end
+                    end
                 end
 
                 %% Use code commeneted out below to skip RF analysis calculations
                 rf_path = [psth_path, '/receptive_field_analysis'];
+
+                nv_path = normalized_variance_analysis(rf_path, animal_name, wanted_events, unique_regions);
+                nv_list = [nv_list; nv_path];
 
                 %% Run if you want to graph all of the PSTHs or comment it out to skip
                 % try
@@ -83,19 +89,19 @@ function [] = main()
                 % end
 
                 %% Run for bootstrapping
-                classified_path = crude_bootstrapper(psth_path, animal_name, boot_iterations, bin_size, pre_time, ...
-                    post_time, wanted_events, wanted_neurons, unit_classification);
+                % classified_path = crude_bootstrapper(psth_path, animal_name, boot_iterations, bin_size, pre_time, ...
+                %     post_time, wanted_events, wanted_neurons, unit_classification);
 
                 %% To skip bootstrapping
                 classified_path = [psth_path, '/classifier'];
 
-                % %% Run for synergy redundancy calculation
-                % % Checks to make sure that both population and unit information exists
-                unit_path = [classified_path, '/unit'];
-                pop_path = [classified_path, '/population'];
-                if (exist(unit_path, 'dir') == 7) && (exist(pop_path, 'dir') == 7)
-                    synergy_redundancy(classified_path, animal_name);
-                end
+                %% Run for synergy redundancy calculation
+                % Checks to make sure that both population and unit information exists
+                % unit_path = [classified_path, '/unit'];
+                % pop_path = [classified_path, '/population'];
+                % if (exist(unit_path, 'dir') == 7) && (exist(pop_path, 'dir') == 7)
+                %     synergy_redundancy(classified_path, animal_name);
+                % end
 
                 % % %% Write to spreadsheet
                 % csv_export(classified_path, original_path, total_events, wanted_events, pre_time, post_time, bin_size, first_iteration, ...
