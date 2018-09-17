833d0fc45a186537da97894cd55b54849a82e55f
Matthew Stickle <mpstickle@ucdavis.edu>
2018-09-17 15:16:43 -0700

    Updated main.m function calls so that it does not directly call the labels function and removed the wanted_neunons from the main.

--- main.m
+++ main.m
@@ -9,8 +9,6 @@ function [] = main()
     total_bins = (length([-abs(pre_time):bin_size:abs(post_time)]) - 1);
     % Requires for all events to be in array. IF empty it will skip all events
     wanted_events = [1, 3, 4, 6];
-    % If wanted_neurons is left empty, it will do all neurons
-    wanted_neurons = [];
     % Inclusive Range
     trial_range = [1, 300];
     % Give exact directory name of the animals you want skipped
@@ -55,7 +53,7 @@ function [] = main()
             elseif isfolder(animal_path)
                 %% Run if you want to parse .plx or comment out to skip
                 % try
-                %     parsed_path = parser(animal_path, animal_name, total_trials, total_events);
+                %    parsed_path = parser(animal_path, animal_name, total_trials, total_events);
                 % end
                 %% Use the code commented out below to skip parsing
                 parsed_path = [animal_path, '/parsed_plx'];
@@ -63,8 +61,7 @@ function [] = main()
                 %% Run if you want to calculate the PSTH or comment it out to skip
                 try
                     psth_path = format_PSTH(parsed_path, animal_name, total_bins, bin_size, pre_time, post_time, ...
-                        wanted_neurons, wanted_events, trial_range, total_trials);
-                    unique_regions = label_neurons(animal_path, psth_path);
+                        wanted_events, trial_range, total_trials);
                 end
                 %% Use code commeneted out below to skip PSTH calculations
                 psth_path = [parsed_path, '/psth'];
@@ -80,8 +77,8 @@ function [] = main()
                 %% Use code commeneted out below to skip RF analysis calculations
                 rf_path = [psth_path, '/receptive_field_analysis'];
 
-                nv_path = normalized_variance_analysis(rf_path, animal_name, wanted_events, unique_regions);
-                nv_list = [nv_list; nv_path];
+                % nv_path = normalized_variance_analysis(rf_path, animal_name, wanted_events, unique_regions);
+                % nv_list = [nv_list; {nv_path}];
 
                 %% Run if you want to graph all of the PSTHs or comment it out to skip
                 % try
@@ -90,7 +87,7 @@ function [] = main()
 
                 %% Run for bootstrapping
                 % classified_path = crude_bootstrapper(psth_path, animal_name, boot_iterations, bin_size, pre_time, ...
-                %     post_time, wanted_events, wanted_neurons, unit_classification);
+                %     post_time, wanted_events, unit_classification);
 
                 %% To skip bootstrapping
                 classified_path = [psth_path, '/classifier'];
@@ -110,5 +107,6 @@ function [] = main()
             end
         end
     end
+    % population_nv(nv_list)
     toc(start_time);
 end
