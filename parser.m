ce626731b351d876d598c38dfc0492ebaa010964
Matthew Stickle <mpstickle@ucdavis.edu>
2019-02-01 12:46:37 -0800

    Cleaned up the parser code and made it parse the variables out of the plx more cleanly. The parser is more generalizable now and can better handle strobed, non strobed, and combined data sets.

--- parser.m
+++ parser.m
@@ -1,4 +1,5 @@
-function [parsed_path] = parser(dir_path, animal_name, total_trials, total_events)
+function [parsed_path] = parser(dir_path, animal_name, total_trials, total_events, trial_lower_bound, ...
+                            is_non_strobed_and_strobed, event_map);
     tic;
     %% Select Directory for debugging purposes
     % dir_path = uigetdir(pwd);
@@ -24,7 +25,7 @@ function [parsed_path] = parser(dir_path, animal_name, total_trials, total_event
        delete([failed_path, '/*']);
        rmdir(failed_path);
     end
-    
+
     % Runs through all of the .plx files in the selected directory
     for h = 1: length(plx_files)
         failed_parsing = {};
@@ -35,6 +36,7 @@ function [parsed_path] = parser(dir_path, animal_name, total_trials, total_event
         % Take the spike times and event times
         try
             try
+                % tscounts and wfcounts dimension is actually (number of channels +1) x (max units  +1)
                 [tscounts, wfcounts, evcounts, slowcounts] = plx_info(file,1);
             catch ME
                 if (strcmpi(ME.identifier,'MATLAB:TooManyOutputs'))
@@ -46,81 +48,57 @@ function [parsed_path] = parser(dir_path, animal_name, total_trials, total_event
                 rethrow(ME);
             end
             fprintf('Parsing for %s on %s\n', animal_name, current_day);
-            
-            [nunits1, nchannels1] = size(tscounts); 
-            % Allocate memory to all_neurons
-            all_neurons = cell(nunits1, nchannels1);
-            for iunit = 1:nunits1 - 1   % starting with unit 0 (unsorted) 
-                for ich = 1:nchannels1 - 1
-                    if (tscounts( iunit+1 , ich+1 ) > 0)
-                        % get the timestamps for this channel and unit 
-                        [nts, all_neurons{iunit+1,ich}] = plx_ts(file, ich , iunit);
+
+            [~, channel_names] = plx_chan_names(file);
+            [total_channels, ~] = plx_chanmap(file);
+            subchan = {'a','b','c','d'};
+            neuron_map = [];
+            for channel = 1:total_channels
+                %% timestamps
+                %TODO deal with unsorted gunk
+                %% goes through the units a, b, c, d
+                for unit = 1:5
+                    % tscounts dimensions: (channel + 1) x (units + 1)
+                    if tscounts(unit + 1, channel + 1) > 0
+                        [~, channel_timestamps] = plx_ts(file, channel , unit);
+                        % channel_names is a char array
+                        current_channel = channel_names(channel, :);
+                        current_subchan = subchan{unit};
+                        complete_channel_name = [current_channel, current_subchan];
+                        neuron_map = [neuron_map; {complete_channel_name}, {channel_timestamps}];
                     end
                 end
             end
-            % Create array for all spikes
-            all_spikes = (all_neurons(~cellfun('isempty',all_neurons)))';
-            total_neurons = length(all_spikes);
-            % All Spikes
-            all_spike_times = [];
-            for i = 1:length(all_spikes)
-                for j = 1:length(all_spikes{1,i})
-                    all_spike_times(i,j) = all_spikes{1,i}(j);
+            total_neurons = length(neuron_map(:,1));
+
+            [total_event_chan, event_channels] = plx_event_chanmap(file);
+            events = [];
+            event_map_counter = 1;
+            for channel = 1:total_event_chan
+                %% strobbed channel is always 257
+                current_channel = event_channels(channel);
+                if current_channel == 257 && evcounts(channel) > 0
+                    is_strobbed = true;
+                    [total_timestamps, event_timestamps, strobed_values] = plx_event_ts(file, current_channel);
+                    events = [strobed_values, event_timestamps];
+                elseif evcounts(channel) > trial_lower_bound
+                    [total_timestamps, event_timestamps, ~] = plx_event_ts(file, current_channel);
+                    total_timestamps = length(event_timestamps);
+                    if is_non_strobed_and_strobed
+                        currenet_event = event_map(event_map_counter);
+                        events = [events; repmat(currenet_event, [total_timestamps, 1]), event_timestamps];
+                        event_map_counter = event_map_counter + 1;
+                    else
+                        events = [events; repmat(current_channel, [total_timestamps, 1]), event_timestamps];
+                    end
                 end
             end
 
-            svStrobed = [];
-            svdummy = [];
-            % and finally the events
-            [u, nevchannels] = size( evcounts );
-            if (nevchannels > 0) 
-                % need the event chanmap to make any sense of these
-                [u,evchans] = plx_event_chanmap(file);
-                for iev = 1:nevchannels
-                    if (evcounts(iev) > 0)
-                        evch = evchans(iev);
-                        if (evch == 257)
-                            [nevs{iev}, tsevs{iev}, svStrobed] = plx_event_ts(file, evch); 
-                        else
-                            [nevs{iev}, tsevs{iev}, svdummy{iev}] = plx_event_ts(file, evch);
-                        end
-                    end
-                end
-            end
-
-            events=[];
-            j = 0;
-            %% Handles strobbed events
-            if length(svStrobed) > 1
-                events = tsevs{1, 17};
-                events = [svStrobed,events];
-            %% Handles nonstrobbed events
-            else
-                for i=1:length(evcounts)
-                    if evcounts(i) >= total_trials
-                        [nevs{i}, tsevs{i}, ~] = plx_event_ts(file, i);
-                        j = j + 1;
-                        eventsingle(1:evcounts(i), 1) = j;
-                        events= [events;eventsingle, tsevs{i}];
-                        eventsingle=[];
-                    end
-                end
-                % TODO potentially use struct solution in calculate_PSTH to relieve hard coding
-                for i=1:length(events)
-                    if events(i,1) == 2
-                        events(i,1) = 3;
-                    elseif events(i,1) == 3
-                        events(i,1) = 4;
-                    elseif events(i,1) == 4
-                        events(i,1) = 6;
-                    end
-                end
-            end
-            %% Removes Doubles and Triples from events
+            %% Removed repeated time stamps
             [events_rows, ~] = size(events);
             count = 1;
             while events_rows > (total_trials * total_events)
-                i = 1;                
+                i = 1;
                 while i <= (length(events)-1)
                     if ((events(i, 2) + 2) > events(i+1, 2))
                         events(i + 1,:) = [];
@@ -136,40 +114,15 @@ function [parsed_path] = parser(dir_path, animal_name, total_trials, total_event
                 end
                 count = count + 1;
             end
-            
-            % Sorts nonstrobbed events. This used to be done before removing
-            % duplicates, but then it caused infinite loops in some nonstrobbed
-            % animals.
-            % TODO figure out why that happens.
-            if length(svStrobed) < 1
-                events = sortrows(events, 2);
-            end
-            
-            %% Create neuron map
-            time_stamp_copy = tscounts;
-            time_stamp_copy(1,:) = [];
-            neuron_map = {};
-            [~,spk_names] = plx_chan_names(file);
-            subchan = ['a','b','c','d'];  
-            for i = 1:length(time_stamp_copy)
-                p = find(time_stamp_copy(:,i));
-                if length(p)>=1
-                    for j = 1:length(p)
-                        channel = deblank(spk_names(i-1, :));
-                        neuron_map(end+1,1) = cellstr([channel,subchan(p(j))]);
-                        % neuron_map(end+1,1) = cellstr([spk_names(i-1,:),subchan(p(j))]);
-                    end
-                end
-            end
-            neuron_map = [neuron_map, all_spikes'];
-            
-            fprintf('Finished parsing for %s\n', current_day);
+
+            events = sortrows(events, 2);
+
             %% Saves parsed files
             % filename = ['PARSED.', file_name, '.mat'];
             filename = [file_name, '.mat'];
             matfile = fullfile(parsed_path, filename);
-            save(matfile, 'tscounts', 'evcounts', 'tsevs', 'events',  ...
-                    'total_neurons', 'all_spike_times', 'neuron_map');
+            save(matfile, 'tscounts', 'evcounts', 'events',  ...
+                    'total_neurons', 'neuron_map');
         catch ME
             if ~exist(failed_path, 'dir')
                 mkdir(dir_path, 'failed');
