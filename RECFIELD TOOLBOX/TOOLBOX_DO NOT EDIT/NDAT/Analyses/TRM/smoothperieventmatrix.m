7bc0a0700238aa8797556327e7ce11605848a890
Matthew Stickle <mpstickle@ucdavis.edu>
2019-02-05 11:16:02 -0800

    Removed the RECFIELD TOOLBOX directory since it was not actually being moved. Temporarily moved out the one function the code does use though (I_confmatr.m) to the directory with the ther functions

--- RECFIELD TOOLBOX/TOOLBOX_DO NOT EDIT/NDAT/Analyses/TRM/smoothperieventmatrix.m
+++
@@ -1,8 +0,0 @@
-function [PeriEventMatrixSmoothed]=smoothperieventmatrix(PeriEventMatrix)
-n=5;
-Neurons = size(PeriEventMatrix,1);
-for i=1:Neurons
-   B=ones(n,1)/n;
-   A=1;
-   PeriEventMatrixSmoothed(i,:)=filter(B,A,PeriEventMatrix(i,:));
-end
