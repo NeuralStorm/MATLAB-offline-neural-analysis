89dc003a602a02b7ca7f29143be707432520395f
nadaamkm <moharramn@gmail.com>
2019-04-08 14:23:46 -0700

    Added a few lines at the bottom

--- utilizedParameterValues.m
+++ utilizedParameterValues.m
@@ -1,5 +1,6 @@
 function [] = utilizedParameterValues(original_path,varargin)
-parameter_array_values = ["Parameter Name", "Parameter Value"];
+
+    parameter_array_values = ["Parameter Name", "Parameter Value"];
     parameter_array_values = cellstr(string(parameter_array_values));
 
     % start at 2 to skip adding the path the function has taken
@@ -23,6 +24,14 @@ parameter_array_values = ["Parameter Name", "Parameter Value"];
         %Create an array loop that adds the variable name and corresponding
         %value to the array
         parameter_array_values = [parameter_array_values; parameter_names(number_of_parameters-1), ...
-                                 cellstr( string( varargin( number_of_parameters-1 )))]; 
+                                 cellstr( string( varargin( number_of_parameters-1 )))];
+    end 
+
+    %creates a table from an array
+    table_of_values = table(parameter_array_values) ;
+    %adds name of csv file to path 
+    table_path = fullfile(original_path,'/Variable Names and Values.csv');
+    %export the table to .csv file
+    writetable(table_of_values,table_path) ;    
 end
 
