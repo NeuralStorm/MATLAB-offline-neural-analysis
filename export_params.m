bc0fa992d9822e31611ecc3a0952cc0c8b9f5a8d
Matthew Stickle <mpstickle@ucdavis.edu>
2019-04-10 11:05:13 -0700

    Exports basic parameters to a csv

--- export_params.m
+++ export_params.m
@@ -1,37 +1,22 @@
-function [] = export_params(original_path,varargin)
-
-    parameter_array_values = ["Parameter Name", "Parameter Value"];
-    parameter_array_values = cellstr(string(parameter_array_values));
-
-    % start at 2 to skip adding the path the function has taken
-    for number_of_parameters = 2:nargin
-        %getting each variable name from main function
-        %substract 1 to start the indexing of variable_names at 1
-        parameter_names{number_of_parameters-1} = inputname(number_of_parameters); 
-
-        %checks the number of arguments in each variable
-        %if it is anything other than 1, must turn into a joined string to be
-        %accepted into the table
-        if numel(varargin {number_of_parameters-1}) ~= 1
-               varargin{number_of_parameters-1} = join( string( varargin{number_of_parameters-1} ));
+function [] = export_params(current_path, varargin)
+    param_table = table;
+    for i = 1:nargin
+        if i == 1
+            current_row = cell2table([{inputname(i)}, {current_path}]);
+        else
+            value = varargin{i - 1};
+            if isstruct(value)
+                current_row = [fieldnames(value), struct2cell(value)];
+                param_table = [param_table; current_row];
+                continue
+            elseif length(value) > 1
+                value = cellstr(num2str(value));
+            end
+            current_row = cell2table([{inputname(i)}, {value}]);
         end
-        %checks if any of the variable values is empty
-        %if it is, the empty brackets is replaced with the string "empty"
-        %This must be included because table does not accept empty values      
-        if isempty( varargin{number_of_parameters-1} )
-            varargin{number_of_parameters-1} = "empty";
-        end
-        %Create an array loop that adds the variable name and corresponding
-        %value to the array
-        parameter_array_values = [parameter_array_values; parameter_names(number_of_parameters-1), ...
-                                 cellstr( string( varargin( number_of_parameters-1 )))];
-    end 
+        param_table = [param_table; current_row];
+    end
 
-    %creates a table from an array
-    table_of_values = table(parameter_array_values) ;
-    %adds name of csv file to path 
-    table_path = fullfile(original_path,'/Variable Names and Values.csv');
-    %export the table to .csv file
-    writetable(table_of_values,table_path) ;    
+    table_path = fullfile(current_path, '/params.csv');
+    writetable(param_table, table_path);
 end
-
