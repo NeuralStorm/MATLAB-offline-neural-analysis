3bac1d4304c7cd4cb352004bdc375c78926f99f9
Matthew Stickle <mpstickle@ucdavis.edu>
2019-07-09 10:03:31 -0700

    Fixed splitting of events so it works with different event trials.

--- psth_functions/split_relative_response.m
+++ psth_functions/split_relative_response.m
@@ -5,19 +5,22 @@ function [event_struct] = split_relative_response(relative_response, col_labels,
 
     % Relative response: T X (N * B)
     tot_labels = length(col_labels);
+    [tot_events, ~] = size(all_events);
     [~, tot_cols] = size(relative_response);
     assert((tot_labels * tot_bins) == tot_cols);
 
     event_start = 1;
     event_end = length(all_events{1, 2});
-    for event_index = 1:length(all_events(:, 1))
+    for event_index = 1:tot_events
         event = all_events{event_index, 1};
         tot_event_trials = length(all_events{event_index, 2});
         event_relative_response = relative_response(event_start:event_end, :);
         event_struct.(event).relative_response = event_relative_response;
         event_struct.(event).psth = sum(event_relative_response, 1) / tot_event_trials;
         event_start = event_end + 1;
-        event_end = event_end + tot_event_trials;
+        if event_index + 1 < tot_events
+            event_end = event_end + length(all_events{event_index + 1, 2});
+        end
         %% Split columns in relative response
         bin_start = 1;
         bin_end = tot_bins;
