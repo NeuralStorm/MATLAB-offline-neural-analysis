56f41a5c91788848322549dea28ad1f5f41ff0f3
Matthew Stickle <mpstickle@ucdavis.edu>
2019-08-15 13:05:02 -0700

    Removed unused fields from psth struct and variables used to create those fields.

--- psth_functions/format_PSTH.m
+++ psth_functions/format_PSTH.m
@@ -1,11 +1,5 @@
 function [psth_struct, event_ts, event_strings] = format_PSTH(...
         event_ts, labeled_data, bin_size, pre_time, post_time, wanted_events, trial_range, trial_lower_bound)
-    if abs(pre_time) > 0
-        pre_time_bins = (length(-abs(pre_time): bin_size: 0)) - 1;
-    else
-        pre_time_bins = 0;
-    end
-    post_time_bins = (length(0:bin_size:post_time)) - 1;
 
     psth_struct = struct;
 
@@ -21,14 +15,6 @@ function [psth_struct, event_ts, event_strings] = format_PSTH(...
         region_neurons = [labeled_data.(region_name)(:,1), labeled_data.(region_name)(:,4)];
         region_response = create_relative_response(region_neurons, psth_struct.all_events, ...
             bin_size, pre_time, post_time);
-
-        for event = 1:length(event_strings)
-            current_event = event_strings{event};
-            current_psth = region_response.(current_event).psth;
-            [pre_time_activity, post_time_activity] = split_psth(current_psth, pre_time, pre_time_bins, post_time_bins);
-            region_response.(current_event).norm_pre_time_activity = pre_time_activity;
-            region_response.(current_event).norm_post_time_activity = post_time_activity;
-        end
         psth_struct.(region_name) = region_response;
     end
 end
