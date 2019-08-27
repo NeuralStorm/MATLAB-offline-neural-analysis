cdced6e16f388d0d3dd4a2129560f3ddacf023fe
sujwang <42629413+Tenger76@users.noreply.github.com>
2019-08-27 10:26:29 -0700

    Added bandpass filter to the pipeline.

---
+++ broad_band_decoder/bandpass_filter_for_boardband.m
@@ -0,0 +1,15 @@
+function bandpass_filtered_map = bandpass_filter_for_boardband(board_band_map, bandpass_filter_order, bandpass_filter_low_fc, bandpass_filter_high_fc, sample_rate)
+    bandpass_filtered_map = [];
+    [tot_channel, ~] = size(board_band_map);
+    for channel_index = 1:tot_channel
+%         [highpass_filtered_data] = butterworth(bandpass_filter_order, ...
+%             [bandpass_filter_low_fc/(sample_rate/2) bandpass_filter_high_fc/(sample_rate/2)], ...
+%             'bandpass', double(board_band_map{channel_index, 2}));
+        [z, p, k] = butter(bandpass_filter_order ,...
+            [bandpass_filter_low_fc/(sample_rate/2) bandpass_filter_high_fc/(sample_rate/2)], 'bandpass');
+        [sos, g] = zp2sos(z, p, k);
+        bandpass_filtered_data = filtfilt(sos, g, board_band_map{channel_index, 2});
+        bandpass_filtered_map = [bandpass_filtered_map; {board_band_map{channel_index, 1}}, ...
+            {bandpass_filtered_data}];
+    end
+end
