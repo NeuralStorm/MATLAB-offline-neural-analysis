099376e412044f59760b4427e723f7eb22f79ea8
Matthew Stickle <mpstickle@ucdavis.edu>
2019-09-12 09:12:55 -0700

    Fixed minor bug with case match and variable name

--- filtering/do_filter.m
+++ filtering/do_filter.m
@@ -10,7 +10,7 @@ function [filtered_map] = do_filter(data_map, sample_rate, notch_filt, ...
         case {'low', 'high'}
             filtered_map = low_high_filt(data_map, sample_rate, filt_type, ...
                 filt_freq, filt_order);
-        case 'band'
+        case 'bandpass'
             assert(length(filt_freq) == 2)
             filtered_map = bandpass_filter_for_boardband(data_map, ...
                 filt_order, filt_freq(1), filt_freq(2), sample_rate);
@@ -18,6 +18,6 @@ function [filtered_map] = do_filter(data_map, sample_rate, notch_filt, ...
             filtered_map = data_map;
             return
         otherwise
-            error('Unsupported type: %s, try low, high, or band instead', type_filt);
+            error('Unsupported type: %s, try low, high, or band instead', filt_type);
     end
 end
