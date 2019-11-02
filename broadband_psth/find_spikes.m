c5baac3ad5d21e5a6dcab00a36bcae3a96e46176
Gary Blumenthal <garyhwbl@gmail.com>
2019-11-01 17:29:43 -0700

    - changed how spikes are found from using derivatives to using the built in matlab function findpeaks - added stimulation artifact removal functionality

--- broadband_psth/find_spikes.m
+++ broadband_psth/find_spikes.m
@@ -1,4 +1,5 @@
-function [spikes] = find_spikes(amp_signal, sd_size, time)
+function [spikes] = find_spikes(amp_signal, sd_size, time, ts, sample_rate,...
+    background_start, background_end, remove_stim_artifact, artifact_rm_multiplier)
 
 total_chan_num = length(amp_signal);
 spikes = struct;
@@ -6,27 +7,47 @@ spikes = struct;
 disp(['Creating spike table...']);
 for current_chan_num = 1:total_chan_num
     
-    temp_spikes = [];
     signal = amp_signal(current_chan_num).data;
-    signal_average = mean(signal);     %takes mean of entire signal, including windows of stim
-    signal_sd = std(signal) * sd_size;       %takes SD of entire signal, including windows of stim
-    threshold = signal_average + signal_sd;
+    background_signal = []; 
+        
+    %Remmoves stimulus artifact
+    %EVENT_SPAN IS HARDCODED - NEEDS TO BE THE DIFF BETWEEN TS(2) - TS(1)
+    %OR AN INPUT
+    if remove_stim_artifact
+        event_span = 0.001;
+        num_samples = event_span * sample_rate; 
+        for event = 1:length(ts)
 
-    spike_time_index = find(diff(signal > threshold) == 1);
-    spike_time = zeros(1,length(spike_time_index));
-    
-    for i=1:length(spike_time_index)
-        spike_time(i)=time(spike_time_index(i));
+            pre_event = ts(1, event) - (num_samples * artifact_rm_multiplier);
+            post_event = ts(2, event) + (num_samples * artifact_rm_multiplier); 
+
+            signal(pre_event:post_event) = nan; 
+
+        end
     end
     
+    %Finds threshold based on background window for spike detection
+    for event = 1:length(ts)
+       
+        thresh_start = ts(1,event) - (abs(background_start) * sample_rate); 
+        thresh_end = ts(1,event) - (abs(background_end) * sample_rate); 
+        background_signal = [background_signal, signal(thresh_start:thresh_end)]; 
+    end
     
-%     for current_sample = 1:length(time)
-%         if amp_signal(current_chan_num, current_sample) > (signal_average + signal_sd) 
-%             temp_spikes = [temp_spikes, time(1,current_sample)]; 
-%         end    
-%     end
-%     
-    spikes(current_chan_num).spike_table = spike_time;
+    %Finds signal average and threshold 
+    signal_average = nanmean(background_signal);     
+    signal_sd = nanstd(background_signal) * sd_size;       
+    threshold = signal_average - signal_sd;
+
+    %Finds spike time stamps
+    %Uses inverse signal and threshold in order to use the findpeaks
+    %function with 'MinPeakHeight' restriction
+    inv_sig = signal * -1;
+    inv_thresh = threshold * -1; 
+    [all_peaks, all_peaks_index] = findpeaks(inv_sig, time, 'MinPeakDistance', 0.001, 'MinPeakHeight', inv_thresh);
+    spikes(current_chan_num).spike_table = all_peaks_index; 
+    
+    
     
 end
 
