2a5f77747eef9fb15c1feb880c4b53cfeffd0a83
Matthew Stickle <mpstickle@ucdavis.edu>
2020-02-21 11:28:43 -0800

    Fixed bug where window end was made with window start making the entire window for slicing symetrical around what window start was set to

--- sep_functions/slice_signal.m
+++ sep_functions/slice_signal.m
@@ -7,12 +7,9 @@ function sliced_signals = slice_signal(data_map, event_samples, ...
     window_end = abs(sep_window(2)) * sample_rate; 
 
     sample_window_start = arrayfun(@(x) (x - window_start), event_samples);
-    %TODO figure out if window_start should be window_end below
-    sample_window_end = arrayfun(@(x) (x + window_start), event_samples); 
+    sample_window_end = arrayfun(@(x) (x + window_end), event_samples); 
     sample_window = [sample_window_start; sample_window_end]; 
-        
-    sliced_signals = struct('channel', data_map(:,1), 'sliced_data', []);
-    
+
     for channel = 1:length(data_map)
         chan_signals = zeros(length(event_samples), (sample_window(2,1) ...
             - sample_window(1,1) + 1));
