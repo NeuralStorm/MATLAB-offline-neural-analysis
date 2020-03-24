ca8692631325919d5d4e2e530892b790f0b24ec2
Matthew Stickle <mpstickle@ucdavis.edu>
2020-03-23 18:11:39 -0700

    Removed requirement to have date column in labels file. Generalized and cleaned up check and enforcement of headers in labels file

--- general_functions/load_labels.m
+++ general_functions/load_labels.m
@@ -1,22 +1,20 @@
-function [label_table] = load_labels(animal_path, csv_id)
-    csv_file = fullfile(animal_path, csv_id);
+function [label_table] = load_labels(dir_path, csv_id)
+    %% Load labels file
+    csv_file = fullfile(dir_path, csv_id);
     if ~isfile(csv_file)
         error('Must have label file to label')
     end
     label_table = readtable(csv_file);
 
-
-
+    %% Gather headers and set expected headers
     label_headers = label_table.Properties.VariableNames;
-    expected_headers = {{'sig_channels'}, {'selected_channels'}, {'user_channels'}, {'label'}, {'label_id'}, ...
-        {'recording_session'}, {'date'}, {'recording_notes'}};
-    if length(label_headers) ~= length(expected_headers)
-        celldisp(expected_headers, 'Expected headers: ');
+    expected_headers = [{'sig_channels'}, {'selected_channels'}, {'user_channels'}, {'label'}, {'label_id'}, ...
+        {'recording_session'}, {'recording_notes'}];
+    logical_headers = ismember(expected_headers, label_headers);
+
+    %% Enforce headers
+    if ~all(logical_headers)
+        celldisp(expected_headers(~logical_headers), 'Expected headers: ');
         error('Must have the above headers in labels');
-    end
-    header_diffs = all(cellfun(@strcmpi, label_headers, expected_headers));
-    if ~header_diffs
-        celldisp(expected_headers(~header_diffs), 'missing headers');
-        error('Must have above headers in csv');
     end
 end
