41daaa7debbd432c3f46f3de06f993eb9e61aa30
Matthew Stickle <mpstickle@ucdavis.edu>
2020-04-02 10:58:49 -0700

    Added functionality that converts type double columns to cell strings so that multi dimensional arrays can be exported to csv

--- general_functions/export_csv.m
+++ general_functions/export_csv.m
@@ -1,15 +1,7 @@
 function [] = export_csv(csv_path, new_results, ignore_columns)
-    %TODO use uniquetol to check uniqueness for doubles?
     new_results = check_notes(new_results);
     headers = new_results.Properties.VariableNames;
     var_types = get_table_types(new_results);
-
-    %% Find all double columns and convert any columns with multi sized arrays to strings
-    %% Convert multi dimensional array to string
-    % test_str = [];
-    % for i = 1:numel(s)
-    %     test_str = [test_str, ' ' num2str(s(i))];
-    % end
 
     if exist(csv_path, 'file')
         results_table = readtable(csv_path);
@@ -19,15 +11,30 @@ function [] = export_csv(csv_path, new_results, ignore_columns)
             var_types, 'VariableNames', headers);
     end
 
+    assert(all(ismember(new_results.Properties.VariableNames, ...
+        results_table.Properties.VariableNames)), ...
+        'Tables Must have the same column headers for proper appending');
+
+    %% Go through double arrays and convert to strong to prevent multidimensional
+    %% appending errors
+    for col_i = 1:width(new_results)
+        curr_col = headers{col_i};
+        curr_type = class(new_results.(curr_col));
+        if ismember(curr_type, 'double')
+            new_results.(curr_col) = cellstr(num2str(new_results.(curr_col)));
+            if strcmpi(class(results_table.(curr_col)), 'double') && ~isempty(results_table.(curr_col))
+                results_table.(curr_col) = cellstr(num2str(results_table.(curr_col)));
+            end
+        end
+    end
+
     %% Append new results to existing results table
     results_table = [results_table; new_results];
-
-    %TODO assert same column headers and column dimensions
 
     %% Creating filter columns to find unique rows in csv
     check_cols = 1:1:width(results_table);
     remove_cols = [];
-    for col_i = 1:width(results_table);
+    for col_i = 1:width(results_table)
         curr_col = headers{col_i};
         if any(ismember(curr_col, ignore_columns))
             %% Check if column is to be ignored
@@ -37,7 +44,8 @@ function [] = export_csv(csv_path, new_results, ignore_columns)
         curr_type = class(results_table.(curr_col));
         if ismember(curr_type, 'double')
             %% Check for NaNs and remove column if it does (NaN != NaN)
-            if any(isnan(results_table.(curr_col)))
+            column_data = results_table.(curr_col);
+            if any(isnan(column_data))
                 remove_cols = [remove_cols, col_i];
             end
         elseif ismember(var_types{col_i}, {'string', 'char'})
@@ -59,13 +67,13 @@ function [] = export_csv(csv_path, new_results, ignore_columns)
 end
 
 function [data_table] = check_notes(data_table)
-    notes_logical = contains(data_table.Properties.VariableNames, 'notes');
+    notes_logical = contains(data_table.Properties.VariableNames, {'notes', 'optional_info'});
     if any(notes_logical)
         note_headers = data_table.Properties.VariableNames(notes_logical);
         for header_i = 1:length(note_headers)
             curr_header = note_headers{header_i};
             if strcmpi(class(data_table.(curr_header)), 'double')
-                data_table.(curr_header) = num2cell(data_table.(curr_header));
+                data_table.(curr_header) = cellstr(num2str(data_table.(curr_header)));
             end
         end
     end
