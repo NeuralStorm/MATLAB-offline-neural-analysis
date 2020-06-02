759083d4ff255faf39e4151ad104faed07cf4156
Matthew Stickle <mpstickle@ucdavis.edu>
2020-06-01 17:18:11 -0700

    Removed power layer from mnts_struct and adapted graphing function for plot weights to expect features only and not a multilayered struct.

--- ieeg_functions/plot_pca_weights.m
+++ ieeg_functions/plot_pca_weights.m
@@ -1,5 +1,6 @@
 function [] = plot_pca_weights(save_path, component_results, label_log, feature_filter, ...
         feature_value, ymax_scale, sub_rows, sub_cols, session_num)
+    %TODO rename to plot_power_features()
 
     color_map = [0 0 0 % black
                 1 0 0 % red
@@ -7,71 +8,122 @@ function [] = plot_pca_weights(save_path, component_results, label_log, feature_
                 0 1 0 % green
                 1 0 1 % magenta
                 1 1 0]; % yellow
+    [tot_colors, ~] = size(color_map);
+    plot_start = 2;
+    plot_increment = 1;
 
-    unique_powers = fieldnames(label_log);
-    for pow_i = 1:length(unique_powers)
-        curr_pow = unique_powers{pow_i};
-        %% Shade different powers in plot
-        if contains(curr_pow, '_')
-            multi_powers = true;
-            split_powers = strsplit(curr_pow, '_');
-            tot_pows = length(split_powers);
-        else
-            multi_powers = false;
-            tot_pows = 1;
-            split_powers = {curr_pow};
+    unique_features = fieldnames(component_results);
+    unique_features = unique_features(~ismember(unique_features, 'all_events'));
+
+    for feature_i = 1:numel(unique_features)
+        feature = unique_features{feature_i};
+
+        %% Establish rows & cols for scrollsubplot, create figure, and title
+        pca_weights = component_results.(feature).coeff;
+        [~, tot_components] = size(pca_weights);
+        if strcmpi(feature_filter, 'pcs') && feature_value < tot_components
+            %% Grabs desired number of principal components weights
+            tot_components = feature_value;
         end
-
-        unique_regions = fieldnames(label_log.(curr_pow));
-        for region_i = 1:length(unique_regions)
-            region = unique_regions{region_i};
-            if contains(region, '_')
-                split_regions = strsplit(region, '_');
-                tot_sub_regs = length(split_regions);
-                multi_regs = true;
-            else
-                multi_regs = false;
-                tot_sub_regs = 1;
-                split_regions = {region};
+        %% Determine if there are too many rows/cols and reduces to make subplot bigger
+        if (sub_cols * sub_rows) > tot_components
+            plot_cols = round(tot_components / 2) + 1;
+            plot_rows = tot_components - plot_cols;
+            if plot_rows < 1
+                plot_rows = 1;
             end
+        else
+            plot_rows = sub_rows;
+            plot_cols = (sub_cols + 1);
+        end
+        %% Create figure
+        figure('visible', 'off')
+        title_txt = strrep(feature, '_', ' ');
+        sgtitle(title_txt);
 
-            [~, tot_components] = size(component_results.(curr_pow).(region).coeff);
-            %% Determine if there are too many rows/cols and reduces to make subplot bigger
-            if (sub_cols * sub_rows) > tot_components
-                plot_cols = round(tot_components / 2) + 1;
-                plot_rows = tot_components - plot_cols;
-                if plot_rows < 1
-                    plot_rows = 1;
+        %% Plot PC variance
+        scrollsubplot(plot_rows, plot_cols, 1);
+        bar(component_results.(feature).component_variance, ...
+            'EdgeColor', 'none');
+        xlabel('PC #');
+        ylabel('% Variance');
+        title('Percent Variance Explained')
+
+        %% Find powers and regions in feature
+        sub_features = strsplit(feature, '_');
+        band_locs = ~ismember(sub_features, label_log.(feature).label);
+        band_list = sub_features(band_locs);
+        region_list = sub_features(~band_locs);
+
+        %% Set color map pairing for each unique region that appears
+        color_struct = struct;
+        color_i = 1;
+        coeff_i = 1;
+        for region_i = 1:numel(region_list)
+            region = region_list{region_i};
+            if isfield(color_struct, region)
+                tot_region_chans = color_struct.(region).tot_region_chans;
+                color_struct.(region).indices = [color_struct.(region).indices, coeff_i:(coeff_i + tot_region_chans - 1)];
+                coeff_i = coeff_i + tot_region_chans;
+            else
+                %% Set indices in coefficients
+                tot_region_chans = numel(label_log.(feature).sig_channels(strcmpi(label_log.(feature).label, region)));
+                color_struct.(region).tot_region_chans = tot_region_chans;
+                color_struct.(region).indices = coeff_i:(coeff_i + tot_region_chans - 1);
+                coeff_i = coeff_i + tot_region_chans;
+                %% set region color
+                color_struct.(region).color = color_map(color_i, :);
+                if color_i == tot_colors
+                    color_i = 1;
+                else
+                    color_i = color_i + 1;
                 end
-            else
-                plot_rows = sub_rows;
-                plot_cols = (sub_cols + 1);
             end
-            %% Create figure
-            figure('visible', 'off')
-            title_txt = strrep([curr_pow ' ' region], '_', ' ');
-            sgtitle(title_txt);
-
-            %% Plot PC variance
-            scrollsubplot(plot_rows, plot_cols, 1);
-            bar(component_results.(curr_pow).(region).component_variance, ...
-                'EdgeColor', 'none');
-            xlabel('PC #');
-            ylabel('% Variance');
-            title('Percent Variance Explained')
-
-            pca_weights = component_results.(curr_pow).(region).coeff;
-            plot_start = 2;
-            plot_increment = 1;
-            region_table = label_log.(curr_pow).(region);
-            plot_weights(pca_weights, ymax_scale, feature_filter, feature_value, ...
-            color_map, multi_regs, tot_sub_regs, split_regions, region_table, multi_powers, tot_pows, split_powers, ...
+        end
+        tot_plots = plot_weights(pca_weights, ymax_scale, color_struct, ...
             plot_rows, plot_cols, plot_start, plot_increment);
 
-            filename = [num2str(session_num), '_', curr_pow, '_', region, '.fig'];
-            set(gcf, 'CreateFcn', 'set(gcbo,''Visible'',''on'')'); 
-            savefig(gcf, fullfile(save_path, filename));
-            close all
+        if numel(band_list) > 1
+            %% Only make power transitions if there are more than 1 power
+            band_splits = struct;
+            shift_i = 0;
+            for loc_i = 1:numel(band_locs)
+                loc_bool = band_locs(loc_i);
+                if loc_bool
+                    if shift_i ~= 0
+                        band_shift = [bandname, '_', sub_features{loc_i}];
+                        band_splits.(band_shift) = shift_i;
+                    end
+                    bandname = sub_features{loc_i};
+                else
+                    region = sub_features{loc_i};
+                    tot_region_chans = numel(label_log.(feature).sig_channels(strcmpi(label_log.(feature).label, region)));
+                    shift_i = shift_i + tot_region_chans;
+                end
+            end
+            unique_shifts = fieldnames(band_splits);
+
+            %% Plot power shifts
+            for comp_i = 2:tot_plots
+                scrollsubplot(plot_rows, plot_cols, comp_i);
+                hold on;
+                for split_i = 1:numel(unique_shifts)
+                    power_shift = unique_shifts{split_i};
+                    % + .5 to center vertical line between bars
+                    xline((band_splits.(power_shift) + 0.5), 'k', ...
+                        strrep(power_shift, '_', ' '), ...
+                        'LabelOrientation', 'horizontal', ...
+                        'LabelHorizontalAlignment', 'center', ...
+                        'HandleVisibility', 'off');
+                end
+                hold off;
+            end
         end
+
+        %% save subplot
+        filename = ['test_', num2str(session_num), '_', feature, '.fig'];
+        set(gcf, 'CreateFcn', 'set(gcbo,''Visible'',''on'')'); 
+        savefig(gcf, fullfile(save_path, filename));
+        close all
     end
 end
--- ieeg_functions/plot_weights.m
+++ ieeg_functions/plot_weights.m
@@ -1,109 +1,40 @@
-function [] = plot_weights(pca_weights, ymax_scale, feature_filter, feature_value, ...
-    color_map, multi_regs, tot_sub_regs, split_regions, region_table, multi_powers, tot_pows, split_powers, ...
-    sub_rows, sub_cols, plot_counter, plot_incrememnt)
-
-    [~, tot_colors] = size(color_map);
+function [tot_plots] = plot_weights(pca_weights, ymax_scale, color_struct, ...
+        sub_rows, sub_cols, plot_counter, plot_increment)
+    %TODO rename to plot_feature_weights()
 
     y_max = max(max(pca_weights)) + (ymax_scale * max(max(pca_weights)));
     y_min = min(min(pca_weights));
     if max(max(pca_weights)) == y_min
         y_min = -y_min;
     end
-    [tot_chans, tot_components] = size(pca_weights);
-    if strcmpi(feature_filter, 'pcs')
-        %% Grabs desired number of principal components weights
-        if feature_value < tot_components
-            tot_components = feature_value;
-        end
-    end
+    [~, tot_components] = size(pca_weights);
+    unique_regions = fieldnames(color_struct);
     for comp_i = 1:tot_components
         comp_weights = pca_weights(:, comp_i);
         scrollsubplot(sub_rows, sub_cols, plot_counter);
+        hold on
         if tot_components == 0
             continue
         end
-        if multi_powers
-            hold on
-            tot_pow_pc = tot_chans / tot_pows;
-            splits = [0, tot_pow_pc:tot_pow_pc:(tot_chans - 1)];
-            if multi_regs
-                % Case: multi powers and regions
-                for split_i = 1:length(splits)
-                    color_counter = 1;
-                    feature_start = splits(split_i) + 1;
-                    reg_start = feature_start;
-                    for reg_i = 1:tot_sub_regs
-                        sub_reg = split_regions{reg_i};
-                        subreg_table = region_table(strcmpi(region_table.label, sub_reg), :);
-                        tot_sub_chans = height(subreg_table);
-                        reg_end = reg_start + tot_sub_chans - 1;
-                        bar(reg_start:reg_end, comp_weights(reg_start:reg_end), ...
-                            'FaceColor', color_map(color_counter, :), ...
-                            'EdgeColor', 'none');
-                        reg_start = reg_end + 1;
-                        if color_counter < tot_colors
-                            color_counter = color_counter + 1;
-                        else
-                            color_counter = 1;
-                        end
-                    end
-                end
-            else
-                % Case: Multi powers, single region
-                bar(comp_weights, 'b', 'EdgeColor', 'none');
-            end
-            %% Add power line marking
-            for split_i = 2:length(splits)
-                feature_split = splits(split_i);
-                % + .5 to center vertical line between bars
-                xline((feature_split + .5), 'k', ...
-                    [split_powers{split_i - 1}, ' ' split_powers{split_i}], ...
-                    'LabelOrientation', 'horizontal', ...
-                    'LabelHorizontalAlignment', 'center', ...
-                    'HandleVisibility', 'off');
-            end
-            hold off
-        elseif multi_regs
-            %% Case multi regions only
-            reg_start = 1;
-            color_counter = 1;
-            for reg_i = 1:tot_sub_regs
-                sub_reg = split_regions{reg_i};
-                subreg_table = region_table(strcmpi(region_table.label, sub_reg), :);
-                tot_sub_chans = height(subreg_table);
-                reg_end = reg_start + tot_sub_chans - 1;
-                bar(reg_start:reg_end, comp_weights(reg_start:reg_end), ...
-                    'FaceColor', color_map(color_counter, :), ...
-                    'EdgeColor', 'none');
-                reg_start = reg_end + 1;
-                if color_counter < tot_colors
-                    color_counter = color_counter + 1;
-                else
-                    color_counter = 1;
-                end
-            end
-        else
-            hold on;
-            bar(comp_weights, ...
-                'FaceColor', color_map(1, :), ...
+        for region_i = 1:numel(unique_regions)
+            region = unique_regions{region_i};
+            reg_i = color_struct.(region).indices;
+            bar(reg_i, comp_weights(reg_i), ...
+                'FaceColor', color_struct.(region).color, ...
                 'EdgeColor', 'none');
-            hold off;
         end
-        %% Creates Legends
-        if multi_regs
-            warning('off','all')
-            lg = legend(split_regions);
-            legend('boxoff');
-            lg.Location = 'Best';
-            lg.Orientation = 'Horizontal';
-            warning('on','all')
-        end
+        lg = legend(unique_regions);
+        legend('boxoff');
+        lg.Location = 'Best';
+        lg.Orientation = 'Horizontal';
 
         ylim([y_min y_max]);
         xlabel('Electrode #');
         ylabel('Coefficient Weight');
         sub_title = strrep(['PC ' num2str(comp_i)], '_', ' ');
         title(sub_title)
-        plot_counter = plot_counter + plot_incrememnt;
+        hold off
+        plot_counter = plot_counter + plot_increment;
     end
+    tot_plots = plot_counter - 1;
 end
