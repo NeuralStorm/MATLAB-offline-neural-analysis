800d3cd1271c2020d23bf6915d6693ac3a14a1ff
Matthew Stickle <mpstickle@ucdavis.edu>
2020-10-28 15:45:02 -0700

    Added LDA function for using lda on separate feature space or grouping pcs across different feature spaces

--- ieeg_functions/lda_classify.m
+++ ieeg_functions/lda_classify.m
@@ -14,40 +14,45 @@ function [results] = lda_classify(psth_struct, label_log)
 
     results = struct;
 
+    relative_response = [];
     unique_features = fieldnames(label_log);
     for feature_i = 1:numel(unique_features)
         %% Go through each feature space to do classification
         feature = unique_features{feature_i};
         %TODO generalize events
-        relative_response = psth_struct.(feature).all.relative_response;
-
-        [tot_trials, ~] = size(relative_response);
-        predicted_events = cell(tot_trials, 1);
-        true_events = cell(tot_trials, 1);
-        parfor trial_i = 1:tot_trials
-            %% Separation of test and train sets
-            test_trial = relative_response(trial_i, :);
-            train_response = relative_response;
-            train_response(trial_i, :) = [];
-            train_labels = labels;
-            train_labels(trial_i) = [];
-
-            %% Train model with data
-            lin_model = fitcdiscr(train_response, train_labels);
-            %% Predict label of left out trial
-            classified_out = predict(lin_model, test_trial);
-
-            predicted_events(trial_i) = classified_out;
-            true_events(trial_i) = labels(trial_i);
-        end
-        confusion_matrix = confusionmat(true_events, predicted_events);
-        correct_trials = cellfun(@strcmp, true_events, predicted_events);
-        results.(feature).confusion_matrix = confusion_matrix;
-        results.(feature).mutual_info = I_confmatr(confusion_matrix);
-        results.(feature).correct_trials = correct_trials;
-        results.(feature).performance = mean(correct_trials);
-        results.(feature).correct_trials = table(true_events, predicted_events, correct_trials, ...
-            'VariableNames', {'true', 'predicted', 'correct'});
+        curr_response = psth_struct.(feature).all.relative_response;
+        %% Grab first component
+        %TODO give parameter to control # of componenets
+        relative_response = [relative_response, curr_response];
     end
+
+    % clear('psth_struct', 'label_log', 'pc_log');
+    [tot_trials, ~] = size(relative_response);
+    predicted_events = cell(tot_trials, 1);
+    true_events = cell(tot_trials, 1);
+    for trial_i = 1:tot_trials
+        %% Separation of test and train sets
+        test_trial = relative_response(trial_i, :);
+        train_response = relative_response;
+        train_response(trial_i, :) = [];
+        train_labels = labels;
+        train_labels(trial_i) = [];
+
+        %% Train model with data
+        lin_model = fitcdiscr(train_response, train_labels, 'SaveMemory', 'on');
+        %% Predict label of left out trial
+        classified_out = predict(lin_model, test_trial);
+
+        predicted_events(trial_i) = classified_out;
+        true_events(trial_i) = labels(trial_i);
+    end
+    confusion_matrix = confusionmat(true_events, predicted_events);
+    correct_trials = cellfun(@strcmp, true_events, predicted_events);
+    results.confusion_matrix = confusion_matrix;
+    results.mutual_info = I_confmatr(confusion_matrix);
+    results.correct_trials = correct_trials;
+    results.performance = mean(correct_trials);
+    results.correct_trials = table(true_events, predicted_events, correct_trials, ...
+        'VariableNames', {'true', 'predicted', 'correct'});
     toc
 end
