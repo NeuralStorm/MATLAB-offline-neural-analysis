04668f76be71cb688f2ba5883e8c73e5d6ec4582
Matthew Stickle <mpstickle@ucdavis.edu>
2020-12-03 14:51:09 -0800

    Removed old commented out code and excess whitespace as well as unused variables. Added config_log to saved output of spike extraction

--- batch_functions/batch_continuous_extract_format_spikes.m
+++ batch_functions/batch_continuous_extract_format_spikes.m
@@ -8,7 +8,7 @@ function batch_continuous_extract_format_spikes(save_path, failed_path, data_pat
     file_list = get_file_list(curr_dir, '.mat');
     file_list = update_file_list(file_list, failed_path, ...
         dir_config.include_sessions);
-    
+
     for file_index = 1:length(file_list)
         [~, filename, ~] = fileparts(file_list(file_index).name);
         filename_meta.filename = filename;
@@ -16,43 +16,35 @@ function batch_continuous_extract_format_spikes(save_path, failed_path, data_pat
         try
             %% Load file contents
             file = [curr_dir, '/', file_list(file_index).name];
-            load(file, 'event_samples', 'filename_meta', 'filtered_map', 'label_log', 'sample_rate');
-            %% Check variables to make sure they are not empty
-%             empty_vars = check_variables(file, sliced_signal);
-%             if empty_vars
-%                 continue
-%             end
-                      
+            load(file, 'event_samples', 'filename_meta', 'filtered_map', 'sample_rate');
             %% Format events for spike extraction and psth formatting
             unique_events = fieldnames(orderfields(event_samples));
-            
+
             event_ts = [];
             sample_ts = event_samples.event_1(1,:);
-            for event_i = 1:length(unique_events)             
-                temp_ts = []; 
+            for event_i = 1:length(unique_events)
+                temp_ts = [];
                 event = unique_events{event_i};
-                
+
                 temp_sample_ts = event_samples.(event)(1,:);
-                
-                temp_ts(:,2) = event_samples.(event)(1,:) / sample_rate; 
+
+                temp_ts(:,2) = event_samples.(event)(1,:) / sample_rate;
                 temp_ts(:,1) = event_i;
-                
-                event_ts = [event_ts; temp_ts];                 
+
+                event_ts = [event_ts; temp_ts];
             end
-            event_ts = sortrows(event_ts, 2); 
-            
+            event_ts = sortrows(event_ts, 2);
+
             %% Extract spikes from filtered data
             channel_map = {};
             for chan = 1:length(filtered_map)
-               
                 channel_map{chan, 1} = filtered_map(chan).sig_channels;
                 [spikes, threshold] = continuous_extract_spikes(filtered_map(chan).data, dir_config.spike_thresh,...
                     sample_ts, sample_rate, dir_config.baseline_start, dir_config.baseline_end);
                 channel_map{chan, 2} = spikes';
-%                 channel_map{chan, 3} = threshold; 
-                
+                % channel_map{chan, 3} = threshold;
             end
-            
+
             %% Label data
             labeled_data = label_data(channel_map, label_table, filename_meta.session_num);
             
@@ -64,14 +56,12 @@ function batch_continuous_extract_format_spikes(save_path, failed_path, data_pat
             end
             
             %% Save spike data
-            save(matfile, '-v7.3', 'channel_map', 'event_ts', 'filename_meta', 'labeled_data');
+            save(matfile, '-v7.3', 'channel_map', 'event_ts', 'filename_meta', 'labeled_data', 'config_log');
             clear('channel_map', 'event_ts', 'filename_meta', 'labeled_data');
             
         catch ME
             handle_ME(ME, failed_path, filename_meta.filename);
         end
-        
-        
     end
 end
 
--- continuous_functions/continuous_extract_spikes.m
+++ continuous_functions/continuous_extract_spikes.m
@@ -1,44 +1,24 @@
 function [spikes, threshold] = continuous_extract_spikes(amp_signal, sd_size, ts, sample_rate, background_start, background_end)
-
-    
-    [total_chan_num, num_samples] = size(amp_signal);
-    spikes = [];
-    amp_time = linspace(0, (length(amp_signal) / sample_rate), length(amp_signal)); 
-  
-    %% Artifact removal: Removes +/- 1 ms of signal around each event ts
-%     one_ms = sample_rate * 0.001; 
-%     for event_i = 1:length(ts)      
-%         start_artifact = ts(event_i) - one_ms;
-%         end_artifact = ts(event_i) + one_ms; 
-%         amp_signal(start_artifact:end_artifact) = nan;         
-%     end
+    amp_time = linspace(0, (length(amp_signal) / sample_rate), length(amp_signal));
 
     %% Calculate threshold used to spike detection
-    background_start_sample = sample_rate * abs(background_start); 
-    background_end_sample = sample_rate * abs(background_end); 
-    background_signals = []; 
-    
+    background_start_sample = sample_rate * abs(background_start);
+    background_end_sample = sample_rate * abs(background_end);
+    background_signals = [];
+
     for i = 1:length(ts)
-        
         background_signals(i,:) = amp_signal((ts(i) - background_start_sample):(ts(i) - background_end_sample));
-    
     end
-    
-    background_average = mean(background_signals, 'all'); 
-    background_std = std(background_signals, 0, 'all'); 
-    threshold = background_average - (background_std * sd_size); 
-    
-%     signal_average = nanmean(amp_signal);     
-%     signal_sd = nanstd(amp_signal) * sd_size;       
-%     threshold = signal_average - signal_sd;
-    
+
+    background_average = mean(background_signals, 'all');
+    background_std = std(background_signals, 0, 'all');
+    threshold = background_average - (background_std * sd_size);
+
     %Finds spike time stamps
     %Uses inverse signal and threshold in order to use the findpeaks
     %function with 'MinPeakHeight' restriction
     inv_sig = amp_signal * -1;
-    inv_thresh = threshold * -1; 
-    [all_peaks, all_peaks_index] = findpeaks(inv_sig, amp_time, 'MinPeakDistance', 0.001, 'MinPeakHeight', inv_thresh);
-    spikes = all_peaks_index; 
-
+    inv_thresh = threshold * -1;
+    [~, all_peaks_index] = findpeaks(inv_sig, amp_time, 'MinPeakDistance', 0.001, 'MinPeakHeight', inv_thresh);
+    spikes = all_peaks_index;
 end
-
