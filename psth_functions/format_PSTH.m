284c70e8105600cd94c2e15e69a6fe1b2b66f037
Matthew Stickle <mpstickle@ucdavis.edu>
2021-02-18 17:44:08 -0800

    Added label_order field to psth_struct and documentation

--- psth_functions/format_PSTH.m
+++ psth_functions/format_PSTH.m
@@ -1,7 +1,23 @@
 function [psth_struct, event_info, label_log] = format_PSTH(event_info, ...
         selected_data, bin_size, window_start, window_end, wanted_events, ...
         trial_range)
-    %TODO add documentation on inputs and outputs
+    %%Inputs
+    % event_info: table with columns event_labels, event_indices, and event_ts
+    % selected_data: struct with fields regions
+    %                region fields: table with information of channels selected for psth
+    % include_events: list of events desired to be analyzed.
+    %                 Must be of same type as event_labels
+    % trial range: numeric range of which trials to be analyzed
+    % window_start: start time of window
+    % window_end: end time of window
+    % bin_size: size of bin
+    %% Output
+    % psth_struct: struct with fields regions
+    %              region fields: relative_response, label_order
+    %              relative_response dimension: Trials (rows) x Neurons * Bins (columns)
+    %              label_order: list of channels in relative response
+    % event_info: event_info table above, but filtered according to wanted_events and trial_range
+    % label_log: similiar to selected_data, but saved as a log of channels
 
     psth_struct = struct;
 
@@ -15,9 +31,11 @@ function [psth_struct, event_info, label_log] = format_PSTH(event_info, ...
     for region_i = 1:length(unique_regions)
         region = unique_regions{region_i};
         region_neurons = [selected_data.(region).sig_channels, selected_data.(region).channel_data];
+        %% create relative response for region neurons
         rr = create_relative_response(region_neurons, event_info.event_ts, bin_edges);
+        %% store relative response and labels in region struct
         psth_struct.(region).relative_response = rr;
-        %TODO add other fields needed for psth_struct
+        psth_struct.(region).label_order = selected_data.(region).sig_channels;
 
         %% Create label log
         region_table = selected_data.(region);
