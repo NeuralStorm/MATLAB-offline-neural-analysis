25e689e07ad470fa05a4235fbd9ecee9da3b61be
Matthew Stickle <mpstickle@ucdavis.edu>
2021-02-24 16:49:21 -0800

    Removed region loop from event struct creation

--- psth_functions/create_event_struct.m
+++ psth_functions/create_event_struct.m
@@ -1,24 +1,20 @@
 function [event_struct] = create_event_struct(psth_struct, event_info, ...
         bin_size, window_start, window_end, new_start, new_end)
 
-    unique_features = fieldnames(psth_struct);
     unique_events = unique(event_info.event_labels);
     event_struct = struct;
-    for feature_i = 1:numel(unique_features)
-        feature = unique_features{feature_i};
-        for event_i = 1:numel(unique_events)
-            event = unique_events{event_i};
-            event_indices = event_info.event_indices(...
-                strcmpi(event_info.event_labels, event));
+    for event_i = 1:numel(unique_events)
+        event = unique_events{event_i};
+        event_indices = event_info.event_indices(...
+            strcmpi(event_info.event_labels, event));
 
-            label_order = psth_struct.(feature).label_order;
-            tot_labels = numel(label_order);
-            rr = psth_struct.(feature).relative_response(event_indices, :);
-            event_response = slice_rr(rr, tot_labels, bin_size, window_start, ...
-                window_end, new_start, new_end);
-            event_struct.(feature).(event).label_order = label_order;
-            event_struct.(feature).(event).relative_response = event_response;
-            event_struct.(feature).(event).psth = calc_psth(event_response, numel(event_indices));
-        end
+        label_order = psth_struct.label_order;
+        tot_labels = numel(label_order);
+        rr = psth_struct.relative_response(event_indices, :);
+        event_response = slice_rr(rr, tot_labels, bin_size, window_start, ...
+            window_end, new_start, new_end);
+        event_struct.(event).label_order = label_order;
+        event_struct.(event).relative_response = event_response;
+        event_struct.(event).psth = calc_psth(event_response, numel(event_indices));
     end
 end
