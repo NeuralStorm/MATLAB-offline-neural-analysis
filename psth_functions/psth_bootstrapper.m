44afd2d625e5bae77b3ed5349f2e50ed08ea8a73
Matthew Stickle <mpstickle@ucdavis.edu>
2021-03-10 17:07:41 -0800

    Removed classification, reestablished channel bootstrapping. Need to reestablish population bootstrapping

--- psth_functions/psth_bootstrapper.m
+++ psth_functions/psth_bootstrapper.m
@@ -1,4 +1,4 @@
-function [res_struct] = psth_bootstrapper(...
+function [chan_perf_info, chan_boot_info] = psth_bootstrapper(...
         psth_struct, event_info, bin_size, window_start, window_end, ...
         response_start, response_end, boot_iterations)
 
@@ -6,108 +6,48 @@ function [res_struct] = psth_bootstrapper(...
     unique_events = unique(event_info.event_labels);
     [~, tot_bins] = get_bins(window_start, window_end, bin_size);
 
-    unit_table = create_table();
-    pop_table = create_table();
-
-    %% Standard classification
+    %% Bootstrapping
     for reg_i = 1:length(unique_regions)
         region = unique_regions{reg_i};
+        chan_order = psth_struct.(region).label_order;
+        tot_chans = numel(chan_order);
+        %% Preallocate arrays before bootstrapping
+        chan_perf_info = prealloc_boot_array(boot_iterations, chan_order);
+        chan_boot_info = prealloc_boot_array(boot_iterations, chan_order);
 
-        %% Population classification
-        event_struct = create_event_struct(psth_struct.(region), event_info, ...
-            bin_size, window_start, window_end, response_start, response_end);
-        res_struct.(region).pop = classify(event_struct, unique_events);
+        parfor i = 1:boot_iterations
+            %% Shuffle labels
+            shuffled_events = event_info;
+            shuffled_events.event_indices = shuffled_events.event_indices(randperm(numel(shuffled_events.event_indices)));
+            shuffled_struct = create_event_response(psth_struct, shuffled_events); %TODO remove regions from create_event_response()
 
-        %% Unit classification
-        tot_features = numel(psth_struct.(region).label_order);
-        feat_s = 1;
-        feat_e = tot_bins;
-        for feat_i = 1:tot_features
-            feature = psth_struct.(region).label_order{feat_i};
-            feat_struct.label_order = {feature};
-            feat_struct.relative_response = psth_struct.(region).relative_response(:, feat_s:feat_e);
             %% Unit classification
-            event_struct = create_event_struct(feat_struct, event_info, ...
-                bin_size, window_start, window_end, response_start, response_end);
-            res_struct.(region).unit.(feature) = classify(event_struct, unique_events);
-            %% Update feature counter
-            feat_s = feat_s + tot_bins;
-            feat_e = feat_e + tot_bins;
+            chan_s = 1;
+            chan_e = tot_bins;
+            for chan_i = 1:tot_chans
+                %TODO add check to skip channels with all the same values
+                %TODO add assert to verify chan_i matches location in preallocated cell arrays
+                chan_struct = struct;
+                chan = chan_order{chan_i};
+                chan_struct.label_order = {chan};
+                chan_struct.relative_response = psth_struct.(region).relative_response(:, chan_s:chan_e);
+                %% Unit classification
+                event_struct = create_event_struct(chan_struct, shuffled_events, ...
+                    bin_size, window_start, window_end, response_start, response_end);
+                res = classify(event_struct, unique_events);
+                chan_perf_info{chan_i, i + 1} = res.performance;
+                chan_boot_info{chan_i, i + 1} = res.mutual_info;
+                %% Update feature counter
+                chan_s = chan_s + tot_bins;
+                chan_e = chan_e + tot_bins;
+            end
+
+            % %% Population classification
+            % [~, shuffled_info, ~, shuffled_perf] = psth_classifier(shuffled_struct.(region), unique_events);
         end
+        avg_chan_info = get_avg_boot(chan_boot_info);
+        avg_chan_perf = get_avg_boot(chan_perf_info);
     end
-
-    %% Bootstrapping
-    % if boot_iterations > 0
-    %     for feature_i = 1:length(unique_features)
-    %         feature = unique_features{feature_i};
-    %         tot_units = numel(psth_struct.(feature).label_order);
-    %         %% Preallocate arrays before bootstrapping
-    %         feature_rand_info = nan(boot_iterations, 2);
-    %         % unit_rand_info = nan(boot_iterations, (tot_units * 2));
-
-    %         parfor i = 1:boot_iterations
-    %             %% Shuffle labels
-    %             shuffled_events = event_info;
-    %             shuffled_events.event_indices = shuffled_events.event_indices(randperm(numel(shuffled_events.event_indices)));
-    %             shuffled_struct = create_event_response(psth_struct, shuffled_events);
-
-    %             %% Unit classification
-    %             % unit_shuffled_info = [];
-    %             % [classify_struct, ~] = classify_unit(feature, selected_data.(feature), ...
-    %             %     shuffled_struct.(feature), unique_events);
-    %             % for unit = 1:length(region_neurons(:,1))
-    %             %     current_unit = region_neurons{unit, 1};
-    %             %     shuffled_info = classify_struct.(feature).(current_unit).mutual_info;
-    %             %     unit_shuffled_info = [unit_shuffled_info, {current_unit}, {shuffled_info}];
-    %             % end
-    %             % region_unit_info = [region_unit_info, unit_shuffled_info];
-
-    %             %% Population classification
-    %             [~, shuffled_info, ~, shuffled_perf] = psth_classifier(shuffled_struct.(feature), unique_events);
-    %             % region_shuffled_info = [region_shuffled_info, {feature}, {shuffled_info}];
-    %             feature_rand_info(i, :) = [shuffled_perf, shuffled_info];
-    %         end
-    %         boot_stats = mean(feature_rand_info);
-    %         pop_struct.(feature).boot_array = feature_rand_info;
-    %         pop_struct.(feature).rand_perf = boot_stats(1);
-    %         pop_struct.(feature).rand_info = boot_stats(2);
-    %     end
-
-    %     % for region = 1:2:length(region_rand_info(1,:))
-    %     %     %% Average region random info and correct classification info
-    %     %     current_region_column = region_rand_info(:, region);
-    %     %     %% Verify that labels are consistent before averaging random info
-    %     %     % TODO throw error but save file info
-    %     %     assert(length(unique(current_region_column)) == 1);
-    %     %     current_region = current_region_column{1};
-    %     %     avg_rand_info = mean([region_rand_info{:, region + 1}]);
-    %     %     corrected_info = pop_table.mutual_info(strcmpi(pop_table.region, current_region)) - avg_rand_info;
-
-    %     %     %% Store region population corrected info and averaged random info
-    %     %     pop_struct.(current_region).avg_rand_info = avg_rand_info;
-    %     %     pop_table.boot_info(strcmpi(pop_table.region, current_region)) = avg_rand_info;
-    %     %     pop_table.corrected_info(strcmpi(pop_table.region, current_region)) = corrected_info;
-
-    %     %     %% Average unit random info and correct classification info
-    %     %     for unit = 1:2:length(unit_rand_info(1,:))
-    %     %         current_unit_column = unit_rand_info(:, unit);
-    %     %         %% Verify that labels are consistent before averaging random info
-    %     %         % TODO throw error but save file info
-    %     %         assert(length(unique(current_unit_column)) == 1);
-    %     %         current_unit = current_unit_column{1};
-    %     %         avg_rand_info = mean([unit_rand_info{:, unit + 1}]);
-    %     %         corrected_info = unit_table.mutual_info(strcmpi(unit_table.region, current_region) ...
-    %     %             & strcmpi(unit_table.sig_channels, current_unit)) - avg_rand_info;
-
-    %     %         %% Store unit corrected info and averaged random info
-    %     %         unit_struct.(current_region).(current_unit).avg_rand_info = avg_rand_info;
-    %     %         unit_table.boot_info(strcmpi(unit_table.region, current_region) ...
-    %     %             & strcmpi(unit_table.sig_channels, current_unit)) = avg_rand_info;
-    %     %         unit_table.corrected_info(strcmpi(unit_table.region, current_region) ...
-    %     %             & strcmpi(unit_table.sig_channels, current_unit)) = corrected_info;
-    %     %     end
-    %     % end
-    % end
 end
 
 function [res] = classify(event_struct, unique_events)
@@ -134,4 +74,14 @@ function [res] = create_table()
     res = table('Size',[0,size(anlysis_columns,1)], ...
                 'VariableNames', anlysis_columns(:,1), ...
                 'VariableTypes', anlysis_columns(:,2));
+end
+
+function [res] = prealloc_boot_array(boot_iterations, chan_order)
+    tot_chans = numel(chan_order);
+    res = cell(tot_chans, boot_iterations + 1);
+    res(:, 1) = chan_order';
+end
+
+function [res] = get_avg_boot(boot_array)
+    res = mean(cell2mat(boot_array(:, 2:end)), 2);
 end
