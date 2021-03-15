71f3b0a6b56816c311c2050acb305e5e6f591ac3
Matthew Stickle <mpstickle@ucdavis.edu>
2021-03-15 14:27:14 -0700

    Updated batch_classify to accomodate changes to psth classifying and bootstrapping. Changed labels to region in table as well

--- batch_functions/batch_classify.m
+++ batch_functions/batch_classify.m
@@ -1,97 +1,94 @@
 function [] = batch_classify(project_path, save_path, failed_path, data_path, dir_name, filename_substring_one, config)
+    %TODO check for uniqueness before running analysis to save computation time more 
     classifier_start = tic;
     config_log = config;
     file_list = get_file_list(data_path, '.mat');
     file_list = update_file_list(file_list, failed_path, config.include_sessions);
 
     %% Pull variable names into workspace scope for log
-    bin_size = config.bin_size; window_start = config.window_start; baseline_start = config.baseline_start;
-    baseline_end = config.baseline_end; window_end = config.window_end; response_start = config.response_start;
-    response_end = config.response_end; bootstrap_classifier = config.bootstrap_classifier;
+    bin_size = config.bin_size; window_start = config.window_start;
+    window_end = config.window_end; response_start = config.response_start;
+    response_end = config.response_end;
     boot_iterations = config.boot_iterations; wanted_events = config.wanted_events;
 
     meta_headers = {'filename', 'animal_id', 'exp_group', 'exp_condition', ...
         'optional_info', 'date', 'record_session', 'bin_size', 'window_start', ...
-        'baseline_start', 'baseline_end', 'window_end', 'response_start', 'response_end', ...
-        'bootstrap_classifier', 'boot_iterations', 'selected_events'};
-    analysis_headers = {'region', 'sig_channels', 'user_channels', 'performance', 'mutual_info', ...
-        'boot_info', 'corrected_info', 'synergy_redundancy', 'synergistic', 'recording_notes'};
+        'response_start', 'response_end', 'window_end', ...
+        'boot_iterations', 'selected_events'};
+    %TODO fix bug that prevents selected_events from being used in checking for uniqueness
     ignore_headers = {'performance', 'mutual_info', 'boot_info', 'corrected_info', ...
-        'synergy_redundancy', 'synergistic'};
+        'synergy_redundancy', 'synergistic', 'selected_events'};
 
     sprintf('PSTH classification for %s \n', dir_name);
 
-    pop_config_info = table;
-    unit_config_info = table;
-    pop_info = [];
-    unit_info = [];
     for file_index = 1:length(file_list)
         [~, filename, ~] = fileparts(file_list(file_index).name);
         filename_meta.filename = filename;
         %% Run through files
         try
             file = fullfile(data_path, file_list(file_index).name);
-            load(file, 'selected_data', 'event_ts', 'response_window', ...
-                'filename_meta', 'label_log');
+            load(file, 'psth_struct', 'event_info', 'filename_meta', 'label_log');
+            %! use label_log to pull the label metadata out (user_chans, recording_notes)
             %% Check psth variables to make sure they are not empty
-            empty_vars = check_variables(file, selected_data, event_ts, response_window);
+            empty_vars = check_variables(file, psth_struct, event_info, label_log);
             if empty_vars
                 continue
             end
 
-            if iscell(wanted_events)
-                wanted_events = wanted_events{:};
-            end
-            if isempty(wanted_events) || all(isnan(wanted_events))
-                wanted_events = [];
-                wanted_events = unique(event_ts(:,1))';
-            end
+            %% Classify
+            [pop_table, chan_table, classify_res] = do_psth_classifier(...
+                psth_struct, event_info, bin_size, window_start, window_end, ...
+                response_start, response_end);
 
-            %% Classify and bootstrap
-            [unit_struct, pop_struct, pop_table, unit_table] = psth_bootstrapper( ...
-                selected_data, response_window, event_ts, wanted_events, ...
-                boot_iterations, bootstrap_classifier, bin_size, ...
-                window_start, baseline_start, baseline_end, window_end, ...
-                response_start, response_end, analysis_headers);
+            %% Bootstrap
+            if boot_iterations > 0
+                [boot_pop, boot_chan] = psth_bootstrapper(psth_struct, ...
+                    event_info, bin_size, window_start, window_end, ...
+                    response_start, response_end, boot_iterations);
+                %% Join classification and boot results
+                pop_table = join(pop_table, boot_pop, 'Keys', 'region');
+                chan_table = join(chan_table, boot_chan, 'Keys', 'channel');
+                %TODO add column for corrected_info
+            else
+                %TODO fill empty columns with NaN
+            end
 
             %% PSTH synergy redundancy
-            [pop_table] = synergy_redundancy(pop_table, unit_table, ...
-                config.bootstrap_classifier);
+            %TODO reimplement
+            % [pop_table] = synergy_redundancy(pop_table, chan_table, ...
+            %     boot_iterations);
 
             %% Add info to results table
-            current_general_info = [
+            meta_data = [
                 {filename_meta.filename}, {filename_meta.animal_id}, ...
                 {filename_meta.experimental_group}, ...
                 {filename_meta.experimental_condition}, ...
                 {filename_meta.optional_info}, filename_meta.session_date, ...
-                filename_meta.session_num, bin_size, window_start, baseline_start, ...
-                baseline_end, window_end, response_start, response_end, ...
-                bootstrap_classifier, boot_iterations, wanted_events
+                filename_meta.session_num, bin_size, window_start, ...
+                response_start, response_end, window_end...
+                boot_iterations, wanted_events
             ];
-            [pop_config_info, pop_info] = ...
-                concat_tables(meta_headers, pop_config_info, current_general_info, pop_info, pop_table);
-            [unit_config_info, unit_info] = ...
-                concat_tables(meta_headers, unit_config_info, current_general_info, unit_info, unit_table);
 
             matfile = fullfile(save_path, ['psth_classifier_', filename_meta.filename, '.mat']);
-            check_variables(matfile, unit_struct, pop_struct, pop_table, unit_table);
-            save(matfile, 'pop_struct', 'unit_struct', 'pop_table', ...
-                'unit_table', 'config_log', 'label_log');
-            clear('unit_struct', 'pop_struct', 'pop_table', 'unit_table', 'label_log');
+            check_variables(matfile, classify_res, pop_table, chan_table);
+            save(matfile, 'classify_res', 'pop_table', 'chan_table', ...
+                'config_log', 'label_log');
+            % clear('classify_res', 'pop_table', 'chan_table', 'label_log');
+
+            %% Add meta data to table before export to csv
+            tot_rows = height(pop_table);
+            pop_table = horzcat_cell(pop_table, repmat(meta_data, [tot_rows, 1]), meta_headers, 'after');
+            tot_rows = height(chan_table);
+            chan_table = horzcat_cell(chan_table, repmat(meta_data, [tot_rows, 1]), meta_headers, 'before');
+            %% Append to CSV
+            pop_csv_path = fullfile(project_path, [filename_substring_one, '_pop_classification_info.csv']);
+            export_csv(pop_csv_path, pop_table, ignore_headers)
+            chan_csv_path = fullfile(project_path, [filename_substring_one,'_chan_classification_info.csv']);
+            export_csv(chan_csv_path, chan_table, ignore_headers);
         catch ME
             handle_ME(ME, failed_path, filename_meta.filename);
         end
     end
-
-    %% CSV set up for unit analysis
-    unit_results = [unit_config_info, unit_info];
-    unit_csv_path = fullfile(project_path, [filename_substring_one,'_unit_classification_info.csv']);
-    export_csv(unit_csv_path, unit_results, ignore_headers)
-    %% CSV set up for pop analysis
-    pop_results = [pop_config_info, pop_info];
-    pop_csv_path = fullfile(project_path, [filename_substring_one, '_pop_classification_info.csv']);
-    export_csv(pop_csv_path, pop_results, ignore_headers)
-
     fprintf('Finished PSTH classifier for %s. It took %s \n', ...
         dir_name, num2str(toc(classifier_start)));
 end
