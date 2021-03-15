932925e7b44aa88f26eb6983bd3071a06f7da083
Matthew Stickle <mpstickle@ucdavis.edu>
2021-03-15 11:07:17 -0700

    Added classify_res struct to store classification results including confusion matrix and trial_log on classification

--- psth_functions/do_psth_classifier.m
+++ psth_functions/do_psth_classifier.m
@@ -1,53 +1,5 @@
-function [pop_table, chan_table] = do_psth_classifier(psth_struct, event_info, bin_size, window_start, window_end, ...
-    response_start, response_end)
-
-    %TODO also return structs with confusion matrix
-    pop_headers = [["label", "string"]; ["performance", "double"]; ...
-                   ["mutual_info", "double"]];
-    chan_headers = [["label", "string"]; ["channels", "string"]; ...
-                    ["performance", "double"]; ["mutual_info", "double"]];
-
-    unique_regions = fieldnames(psth_struct);
-    unique_events = unique(event_info.event_labels);
-    [~, tot_bins] = get_bins(response_start, response_end, bin_size);
-
-    [pop_table, chan_table] = create_table();
-
-    for reg_i = 1:length(unique_regions)
-        region = unique_regions{reg_i};
-        chan_order = psth_struct.(region).label_order;
-        tot_chans = numel(chan_order);
-
-        event_struct = create_event_struct(psth_struct.(region), event_info, ...
-            bin_size, window_start, window_end, response_start, response_end);
-
-        %% Unit classification
-        chan_s = 1;
-        chan_e = tot_bins;
-        for chan_i = 1:tot_chans
-            chan = chan_order{chan_i};
-            %% slice channel from shuffled event struct
-            chan_struct = slice_event_channels(event_struct, chan_s, chan_e);
-            [~, mutual_info, ~, perf] = psth_classifier(chan_struct, unique_events);
-            %% Add channel classifier info to chan table
-            a = [{region}, {chan}, perf, mutual_info];
-            chan_table = concat_cell(chan_table, a, chan_headers(:, 1));
-            %% Update channel counter
-            chan_s = chan_s + tot_bins;
-            chan_e = chan_e + tot_bins;
-        end
-
-        %% Population classification
-        [~, mutual_info, ~, perf] = psth_classifier(event_struct, unique_events);
-
-        %% Add region classifier to pop table
-        a = [{region}, perf, mutual_info];
-        pop_table = concat_cell(pop_table, a, pop_headers(:, 1));
-
-    end
-end
-
-function [pop_table, chan_table] = create_table()
+function [pop_table, chan_table, classify_res] = do_psth_classifier(psth_struct, event_info, bin_size, window_start, window_end, ...
+        response_start, response_end)
     %% Create population table
     pop_headers = [["label", "string"]; ["performance", "double"]; ...
                    ["mutual_info", "double"]];
@@ -60,4 +12,45 @@ function [pop_table, chan_table] = create_table()
     chan_table = table('Size',[0, 4], ...
         'VariableNames', chan_headers(:, 1), ...
         'VariableTypes', chan_headers(:, 2));
+    %% Create struct to store all results from classifier
+    classify_res = struct;
+
+    unique_regions = fieldnames(psth_struct);
+    unique_events = unique(event_info.event_labels);
+    [~, tot_bins] = get_bins(response_start, response_end, bin_size);
+
+    for reg_i = 1:length(unique_regions)
+        region = unique_regions{reg_i};
+        chan_order = psth_struct.(region).label_order;
+        tot_chans = numel(chan_order);
+
+        event_struct = create_event_struct(psth_struct.(region), event_info, ...
+            bin_size, window_start, window_end, response_start, response_end);
+
+        %% Population classification
+        [conf, mutual_info, trial_log, perf] = psth_classifier(event_struct, unique_events);
+        %% Store classification results in table and struct
+        a = [{region}, perf, mutual_info];
+        pop_table = concat_cell(pop_table, a, pop_headers(:, 1));
+        classify_res.(region) = struct('confusion_matrix', conf, ...
+            'mutual_info', mutual_info, 'performance', perf, 'trial_log', trial_log);
+
+        %% Unit classification
+        chan_s = 1;
+        chan_e = tot_bins;
+        for chan_i = 1:tot_chans
+            chan = chan_order{chan_i};
+            %% slice channel from shuffled event struct
+            chan_struct = slice_event_channels(event_struct, chan_s, chan_e);
+            [conf, mutual_info, trial_log, perf] = psth_classifier(chan_struct, unique_events);
+            %% Store classification results in table and struct
+            a = [{region}, {chan}, perf, mutual_info];
+            chan_table = concat_cell(chan_table, a, chan_headers(:, 1));
+            classify_res.(region).(chan) = struct('confusion_matrix', conf, ...
+                'mutual_info', mutual_info, 'performance', perf, 'trial_log', trial_log);
+            %% Update channel counter
+            chan_s = chan_s + tot_bins;
+            chan_e = chan_e + tot_bins;
+        end
+    end
 end
