3a6eb733f2660574c338fc41eb2226f214a484ed
Matthew Stickle <mpstickle@ucdavis.edu>
2021-05-14 12:33:44 -0700

    cleaned up slicing seps from sampled data and made changes to accomodate data structure changes

--- batch_functions/batch_format_sep.m
+++ batch_functions/batch_format_sep.m
@@ -4,6 +4,8 @@ function [] = batch_format_sep(save_path, failed_path, data_path, dir_name, dir_
     config_log = dir_config;
     file_list = get_file_list(data_path, '.mat');
     file_list = update_file_list(file_list, failed_path, dir_config.include_sessions);
+    %% Remove unselected channels
+    label_table(label_table.selected_channels == 0, :) = [];
 
     if dir_config.use_raw
         for file_index = 1:length(file_list)
@@ -12,28 +14,14 @@ function [] = batch_format_sep(save_path, failed_path, data_path, dir_name, dir_
             try
                 %% pull info from filename and set up file path for analysis
                 file = fullfile(data_path, file_list(file_index).name);
-                load(file, 'labeled_data', 'filename_meta', 'event_samples', 'sample_rate');
-                %% Select channels
-                selected_data = select_data(labeled_data, label_table, ...
-                    filename_meta.session_num);
+                load(file, 'channel_map', 'filename_meta', 'event_samples', 'sample_rate');
+                %% Select channels and label data
+                selected_chans = label_data(channel_map, label_table, filename_meta.session_num);
+                chan_group_log = selected_chans;
+                chan_group_log = removevars(chan_group_log, 'channel_data');
 
-                % Reformat parsed data into data structure for SEP formatting
-                unique_regions = fieldnames(selected_data);
-                data_table = table;
-                label_log = struct;
-                for region_i = 1:length(unique_regions)
-                    region = unique_regions{region_i};
-                    region_table = selected_data.(region);
-                    region_log = region_table(:, ~strcmpi(region_table.Properties.VariableNames, 'channel_data'));
-                    label_log.(region) = region_log;
-                    region_table = removevars(region_table, ...
-                        {'selected_channels', 'recording_session', 'date', 'recording_notes'});
-                    data_table = [data_table; region_table];
-                end
-                data_table.Properties.VariableNames = {'sig_channels', ...
-                    'user_channels', 'label', 'label_id', 'data'};
                 %% unfiltered data is called filtered for convience of calling slicing function
-                raw_map = table2struct(data_table);
+                raw_map = table2struct(selected_chans);
     
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 %%          Slicing           %%
@@ -46,8 +34,8 @@ function [] = batch_format_sep(save_path, failed_path, data_path, dir_name, dir_
                 %% Save sep
                 matfile = fullfile(save_path, ['sliced_', filename_meta.filename, '.mat']);
                 save(matfile, '-v7.3', 'sliced_signal', 'sep_window', ...
-                    'config_log', 'filename_meta', 'event_samples', 'label_log');
-                clear('sliced_signal', 'sep_window', 'filename_meta', 'event_samples', 'label_log');
+                    'config_log', 'filename_meta', 'event_samples', 'chan_group_log');
+                clear('sliced_signal', 'sep_window', 'filename_meta', 'event_samples', 'chan_group_log');
             catch ME
                 handle_ME(ME, failed_path, filename_meta.filename);
             end
@@ -61,21 +49,21 @@ function [] = batch_format_sep(save_path, failed_path, data_path, dir_name, dir_
                 file = fullfile(data_path, file_list(file_index).name);
     
                 %% Load needed variables from psth and does the receptive field analysis
-                load(file, 'filtered_map', 'event_samples', 'sample_rate', 'filename_meta', 'label_log');
+                load(file, 'filtered_map', 'event_samples', 'sample_rate', 'filename_meta', 'chan_group_log');
     
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 %%          Slicing           %%
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                sep_window = [-abs(dir_config.window_start), dir_config.window_end];
+                %! change handling of time windows
                 sliced_signal = format_sep(filtered_map, event_samples, ...
-                    sample_rate, sep_window, dir_config.square_pulse, ...
-                    dir_config.include_events);
+                    sample_rate, dir_config.window_start, dir_config.window_end, ...
+                    dir_config.square_pulse, dir_config.include_events);
 
                 %% Save sep
                 matfile = fullfile(save_path, ['sliced_', filename_meta.filename, '.mat']);
-                save(matfile, '-v7.3', 'sliced_signal', 'sep_window', ...
-                    'config_log', 'filename_meta', 'event_samples', 'label_log');
-                clear('sliced_signal', 'sep_window', 'filename_meta', 'event_samples', 'label_log');
+                save(matfile, '-v7.3', 'sliced_signal', ...
+                    'config_log', 'filename_meta', 'event_samples', 'chan_group_log');
+                clear('sliced_signal', 'filename_meta', 'event_samples', 'chan_group_log');
             catch ME
                 handle_ME(ME, failed_path, filename_meta.filename);
             end
--- sep_functions/format_sep.m
+++ sep_functions/format_sep.m
@@ -1,8 +1,7 @@
-function sliced_signals = format_sep(data_map, event_samples, ...
-        sample_rate, sep_window, square_pulse, include_events)
+function data_map = format_sep(data_map, event_samples, ...
+        sample_rate, window_start, window_end, square_pulse, include_events)
 
     unique_events = fieldnames(event_samples);
-    sliced_signals = struct([]);
     for event_i = 1:length(unique_events)
         event = unique_events{event_i};
 
@@ -23,29 +22,12 @@ function sliced_signals = format_sep(data_map, event_samples, ...
             event_ts = event_samples.(event)(1,:);
         end
 
-        window_start = abs(sep_window(1)) * sample_rate; 
-        window_end = abs(sep_window(2)) * sample_rate; 
-
-        sample_window_start = arrayfun(@(x) (x - window_start), event_ts);
-        sample_window_end = arrayfun(@(x) (x + window_end), event_ts);
-        sample_window = [sample_window_start; sample_window_end];
-
-        event_map = data_map;
-        for channel = 1:length(data_map)
-            %% Initialize sep formatted data for event
-            chan_signals = zeros(length(event_ts), (sample_window(2,1) ...
-                - sample_window(1,1) + 1));
-            %% Go through each trial and slice out sep
-            for trial = 1:length(sample_window)
-                chan_signals(trial,:) = data_map(channel).data( ...
-                    sample_window(1, trial):sample_window(2, trial));
-            end
-            %% Replace data with sep data
-            event_map(channel).data = chan_signals;
-        end
-        [event_map(:).event] = deal({event});
-
-        %% Append event data to sliced signals
-        sliced_signals = [sliced_signals; event_map];
+        %% Create array of all trial samples based on event times
+        trial_indices = arrayfun(@(x) ...
+            (x + (window_start*sample_rate)):(x+(window_end*sample_rate)), ...
+            event_ts, 'UniformOutput', false);
+        trial_indices = horzcat(trial_indices{:});
+        %% Slice SEPs
+        data_map.channel_data = data_map.channel_data(:, trial_indices);
     end
 end
