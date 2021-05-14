89eb7d546c7d334c88f6034afa8eaef4273e0342
Matthew Stickle <mpstickle@ucdavis.edu>
2021-05-13 17:25:51 -0700

    Changes to how channel data is stored to be consistent with rest of code base. Also updated channel headers

--- parsing_functions/rh_parser.m
+++ parsing_functions/rh_parser.m
@@ -1,27 +1,33 @@
 function [] = rh_parser(parsed_path, failed_path, raw_file, config, label_table)
     try
+        %% Get filename info
+        [~, filename, ~] = fileparts(raw_file);
+        filename_meta = get_filename_info(filename);
         %% reads and maps data
         if strcmpi(config.rh, 'rhs')
-            [amplifier_channels, amplifier_data, board_adda_channels, board_adda_data, ...
+            [amp_chan_meta, amplifier_data, board_adda_channels, board_adda_data, ...
                 board_dig_in_data, t_amplifier, sample_rate] = read_Intan_RHS2000_file(raw_file);
         elseif strcmpi(config.rh, 'rhd')
-            [amplifier_channels, amplifier_data, board_adda_channels, board_adda_data, ...
+            [amp_chan_meta, amplifier_data, board_adda_channels, board_adda_data, ...
                 board_dig_in_data, t_amplifier, sample_rate] = read_Intan_RHD2000_file(raw_file);
         else
             error('Expected rhs or rhd files, but given %s', config.rh);
         end
-        [~, tot_amp_channels] = size(amplifier_channels);
-        wideband_map = cell(tot_amp_channels, 2);
+        %% Store channel data in table
+        [~, tot_chans] = size(amp_chan_meta);
+        a = cell(tot_chans, 2);
+        for chan_i = 1:tot_chans
+            a(chan_i, :) = [{strrep(amp_chan_meta(chan_i).native_channel_name, '-', '_')}, ...
+                amplifier_data(chan_i, :)];
+        end
+        channel_map = cell2table(a, 'VariableNames', ["channel", "channel_data"]);
+
+        %% enforce labels is all inclusive
+        session_table = label_table(label_table.recording_session == filename_meta.session_num, :);
+        enforce_labels(channel_map.channel, session_table.channel, filename_meta.session_num)
+
         [~, tot_adda_channels] = size(board_adda_channels);
         analog_input_map = cell(tot_adda_channels, 2);
-        % amplifier data mapping
-        for channel = 1:tot_amp_channels
-            wideband_map(channel, :) = [ ...
-                {amplifier_channels(channel).native_channel_name}, ...
-                {amplifier_data(channel, :)} ...
-            ];
-        end
-        wideband_map(:, 1) = cellfun(@(x) strrep(x, '-', '_'), wideband_map(:, 1), 'UniformOutput',false);
         % ad/da data mapping
         for channel = 1:tot_adda_channels
             analog_input_map = [ ...
@@ -30,26 +36,15 @@ function [] = rh_parser(parsed_path, failed_path, raw_file, config, label_table)
             ];
         end
 
-        %% label channel map
-        [~, filename, ~] = fileparts(raw_file);
-        filename_meta = get_filename_info(filename);
-
         event_samples = find_event_samples(board_dig_in_data);
-
-        labeled_data = label_data(wideband_map, label_table, ...
-            filename_meta.session_num);
-
-        channel_list = wideband_map(:, 1);
-        label_list = label_table.sig_channels(filename_meta.session_num == label_table.recording_session);
-        enforce_labels(channel_list, label_list, filename_meta.session_num);
 
         %% Saves parsed files
         matfile = fullfile(parsed_path, [filename, '.mat']);
         save(matfile, '-v7.3', 'analog_input_map', ...
             'board_dig_in_data', 't_amplifier', 'sample_rate', ...
-            'filename_meta', 'labeled_data', 'event_samples');
+            'filename_meta', 'channel_map', 'event_samples');
         clear('analog_input_map', 'board_dig_in_data', 't_amplifier', ...
-            'sample_rate', 'filename_meta', 'labeled_data', 'event_samples');
+            'sample_rate', 'filename_meta', 'channel_map', 'event_samples');
     catch ME
         handle_ME(ME, failed_path, filename);
     end
