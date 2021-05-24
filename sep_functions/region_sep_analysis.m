ce4cb165235b6f186ecc864e7ce7d0973af585e1
Matthew Stickle <mpstickle@ucdavis.edu>
2021-05-24 16:59:05 -0700

    Updated normalization of peaks to group by chan_group and event. Wrote function to reduce repetitive code in normalization as well

--- sep_functions/region_sep_analysis.m
+++ sep_functions/region_sep_analysis.m
@@ -1,42 +1,47 @@
-function [sep_analysis_results] = region_sep_analysis(sep_analysis_results)
-    %! why are we normalizing across both channel groups AND events???
-    unique_ch_groups = unique([sep_analysis_results.chan_group]);
+function [sep_res] = region_sep_analysis(sep_res)
+    unique_ch_groups = unique([sep_res.chan_group]);
+    unique_events = unique([sep_res.event]);
     for ch_group_i = 1:length(unique_ch_groups)
         ch_group = unique_ch_groups{ch_group_i};
-        region_indices = find([sep_analysis_results.chan_group] == ch_group);
-        % Normalized neg and pos peaks to the largest peak within region /
-        %% Negative peak norm
-        if ~isempty([sep_analysis_results(region_indices).neg_peak1])
-            min_peak = min(cell2mat({sep_analysis_results(region_indices).neg_peak1}));
-            norm_neg_peak1 = num2cell([sep_analysis_results(region_indices).neg_peak1] / min_peak * 100);
-            [sep_analysis_results(region_indices).norm_neg_peak1] = norm_neg_peak1{1,:};
-        end
-        if ~isempty([sep_analysis_results(region_indices).neg_peak2])
-            min_peak = min(cell2mat({sep_analysis_results(region_indices).neg_peak2}));
-            norm_neg_peak2 = num2cell([sep_analysis_results(region_indices).neg_peak2] / min_peak * 100);
-            [sep_analysis_results(region_indices).norm_neg_peak2] = norm_neg_peak2{1,:};
-        end
-        if ~isempty([sep_analysis_results(region_indices).neg_peak3])
-            min_peak = min(cell2mat({sep_analysis_results(region_indices).neg_peak3}));
-            norm_neg_peak3 = num2cell([sep_analysis_results(region_indices).neg_peak3] / min_peak * 100);
-            [sep_analysis_results(region_indices).norm_neg_peak3] = norm_neg_peak3{1,:};
-        end
-        %% Positive peak norm
-        if ~isempty([sep_analysis_results(region_indices).pos_peak1])
-            max_peak = max(cell2mat({sep_analysis_results(region_indices).pos_peak1}));
-            norm_pos_peak1 = num2cell([sep_analysis_results(region_indices).pos_peak1] / max_peak * 100);
-            [sep_analysis_results(region_indices).norm_pos_peak1] = norm_pos_peak1{1,:};
-        end
-        if ~isempty([sep_analysis_results(region_indices).pos_peak2])
-            max_peak = max(cell2mat({sep_analysis_results(region_indices).pos_peak2}));
-            norm_pos_peak2 = num2cell([sep_analysis_results.pos_peak2] / max_peak * 100);
-            [sep_analysis_results(region_indices).norm_pos_peak2] = norm_pos_peak2{1,:};
-        end
-        if ~isempty([sep_analysis_results(region_indices).pos_peak3])
-            max_peak = max(cell2mat({sep_analysis_results(region_indices).pos_peak3}));
-            norm_pos_peak3 = num2cell([sep_analysis_results(region_indices).pos_peak3] / max_peak * 100);
-            [sep_analysis_results(region_indices).norm_pos_peak3] = norm_pos_peak3{1,:};
+        for event_i = 1:numel(unique_events)
+            event = unique_events{event_i};
+            chan_i = ismember([sep_res.chan_group], ch_group) & ...
+                        ismember([sep_res.event], event);
+            % Normalized neg and pos peaks to the largest peak within region & event
+            %% Negative peak norm
+            if ~isempty([sep_res(chan_i).neg_peak1])
+                sep_res = norm_peaks(sep_res, chan_i, 'neg_peak1', 'min');
+            end
+            if ~isempty([sep_res(chan_i).neg_peak2])
+                sep_res = norm_peaks(sep_res, chan_i, 'neg_peak2', 'min');
+            end
+            if ~isempty([sep_res(chan_i).neg_peak3])
+                sep_res = norm_peaks(sep_res, chan_i, 'neg_peak3', 'min');
+            end
+            %% Positive peak norm
+            if ~isempty([sep_res(chan_i).pos_peak1])
+                sep_res = norm_peaks(sep_res, chan_i, 'pos_peak1', 'max');
+            end
+            if ~isempty([sep_res(chan_i).pos_peak2])
+                sep_res = norm_peaks(sep_res, chan_i, 'pos_peak2', 'max');
+            end
+            if ~isempty([sep_res(chan_i).pos_peak3])
+                sep_res = norm_peaks(sep_res, chan_i, 'pos_peak3', 'max');
+            end
         end
     end
 end
 
+function [sep_res] = norm_peaks(sep_res, chan_i, field_name, peak_type)
+    if strcmpi(peak_type, 'max')
+        max_peak = max(cell2mat({sep_res(chan_i).(field_name)}));
+        norm_res = num2cell([sep_res(chan_i).(field_name)] / max_peak);
+        [sep_res(chan_i).(['norm_', field_name])] = norm_res{1,:};
+    elseif strcmpi(peak_type, 'min')
+        min_peak = min(cell2mat({sep_res(chan_i).(field_name)}));
+        norm_res = num2cell([sep_res(chan_i).(field_name)] / min_peak);
+        [sep_res(chan_i).(['norm_', field_name])] = norm_res{1,:};
+    else
+        error('Invalid peak_type %s, try max or min instead', peak_type);
+    end
+end
