35755c776326d0ab6d317c639aea963a86ad4b1e
Matthew Stickle <mpstickle@ucdavis.edu>
2021-06-17 19:09:54 -0700

    Removed parfor loop since it turned out to be slower with findpeaks after more testing was done

--- sep_functions/create_channel_map.m
+++ sep_functions/create_channel_map.m
@@ -3,7 +3,7 @@ function [channel_map] = create_channel_map(data_table, event_info, sample_rate,
 
     headers = [["channel", "cell"]; ["channel_data", "cell"]];
     channel_map = prealloc_table(headers, [height(data_table), size(headers, 1)]);
-    parfor chan_i = 1:height(data_table)
+    for chan_i = 1:height(data_table)
         chan = data_table.channel{chan_i};
         data = data_table.channel_data(chan_i, :);
         assert(size(data, 1) == 1, 'Findpeaks requires vector, not matrix');
