f2bcf5ca4b11de53f07bd07844518093524120a0
Matthew Stickle <mpstickle@ucdavis.edu>
2021-06-22 12:02:19 -0700

    Updated plotting sep code to handle events. Now a plot is made for every chan group x event combo

--- sep_functions/graph_sep.m
+++ sep_functions/graph_sep.m
@@ -3,42 +3,44 @@ function [] = graph_sep(save_path, sep_struct, filename_meta, chan_group_log, ..
     %% Set x axis (time)
     time = linspace(sep_struct(1).sep_window(1), sep_struct(1).sep_window(2), ...
         length(sep_struct(1).sep_sliced_data));
-    unique_regions = unique(chan_group_log.chan_group); 
-    for region_i = 1:length(unique_regions)
-        region = unique_regions{region_i};
-        region_table = chan_group_log(strcmpi(chan_group_log.chan_group, region), :);
+    unique_ch_groups = unique([sep_struct.chan_group]);
+    unique_events = unique([sep_struct.event]);
+    for ch_group_i = 1:length(unique_ch_groups)
+        ch_group = unique_ch_groups{ch_group_i};
+        region_table = chan_group_log(strcmpi(chan_group_log.chan_group, ch_group), :);
         region_table = sortrows(region_table, 'user_channels');
+        for event_i = 1:numel(unique_events)
+            event = unique_events{event_i};
+            %% extracts data form chan_group and event
+            a = sep_struct(strcmpi([sep_struct.chan_group], ch_group) ...
+                & strcmpi([sep_struct.event], event));
 
-        %% extracts data for the current region
-        region_list = {sep_struct.chan_group};
-        region_index = cellfun(@(x)contains(x, region), region_list, 'UniformOutput', 1);
-        region_data = sep_struct(region_index);
-        region_sep = [region_data.sep_sliced_data];
+            %% Get global y limits
+            global_sep = [a.sep_sliced_data];
+            y_max = max(global_sep);
+            y_min = min(global_sep);
+            % Add buffer so that y limits don't appear cut off in plots
+            y_max = y_max + (y_max * .002) + eps;
+            y_min = y_min + (y_min *.002) + eps;
 
-        %% Get global y limits
-        y_max = max(region_sep);
-        y_min = min(region_sep);
-        % Add buffer so that y limits don't appear cut off in plots
-        y_max = y_max + (y_max * .002) + eps;
-        y_min = y_min + (y_min *.002) + eps;
-
-        %% Plot each channel in scroll subplot
-        figure('visible', 'off');
-        sgtitle([filename_meta.animal_id, ' ', region]);
-        for chan_i = 1:height(region_table)
-            curr_chan = region_table.channel{chan_i};
-            scrollsubplot(sub_rows, sub_cols, chan_i);
-            sep_data = region_data(strcmpi([region_data.channel], curr_chan)).sep_sliced_data;
-            plot(time, sep_data);
-            hold on;
-            ylim([y_min, y_max]);
-            title(curr_chan);
+            %% Plot each channel in scroll subplot
+            figure('visible', 'off');
+            sgtitle([filename_meta.animal_id, ' ', ch_group]);
+            for chan_i = 1:height(region_table)
+                chan = region_table.channel{chan_i};
+                scrollsubplot(sub_rows, sub_cols, chan_i);
+                sep_data = a(strcmpi([a.channel], chan)).sep_sliced_data;
+                plot(time, sep_data);
+                hold on;
+                ylim([y_min, y_max]);
+                title(chan);
+            end
+            %% Save fig
+            filename = [filename_meta.filename, '_', ch_group, '_', event, '.fig'];
+            % Set CreateFcn callback
+            set(gcf, 'CreateFcn', 'set(gcbo,''Visible'',''on'')');
+            savefig(gcf, fullfile(save_path, filename));
+            close gcf
         end
-        %% Save fig
-        filename = [filename_meta.filename, '_', region, '.fig'];
-        % Set CreateFcn callback
-        set(gcf, 'CreateFcn', 'set(gcbo,''Visible'',''on'')');
-        savefig(gcf, fullfile(save_path, filename));
-        close gcf
     end
 end
